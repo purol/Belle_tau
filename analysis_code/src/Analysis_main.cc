7d052b9835bb2c213206256bee87a4465029daee
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-12-23 12:53:40 +0900

    fix analysis main

--- analysis_code/src/Analysis_main.cc
+++ analysis_code/src/Analysis_main.cc
@@ -25,22 +25,29 @@ int main(int argc, char* argv[]) {
 
     loader.PrintInformation("========== initial ==========");
 
-    loader.Cut("(-0.3 < deltaE) && (deltaE < 0.2)");
-    loader.PrintInformation("========== -0.3 < deltaE < 0.2 ==========");
+    loader.Cut("(-0.296904 < deltaE) && (deltaE < 0.20147)");
+    loader.PrintInformation("========== -0.296904 < deltaE < 0.20147 ==========");
 
-    loader.Cut("(1.68 < (E*E-px*px-py*py-pz*pz)^0.5) && ((E*E-px*px-py*py-pz*pz)^0.5 < 1.86)");
-    loader.PrintInformation("========== 1.68 < M < 1.86 ==========");
+    loader.Cut("(1.681298 < (E*E-px*px-py*py-pz*pz)^0.5) && ((E*E-px*px-py*py-pz*pz)^0.5 < 1.8656)");
+    loader.PrintInformation("========== 1.681298 < M < 1.8656 ==========");
 
-    loader.DrawTH2D("(E*E-px*px-py*py-pz*pz)^0.5", "deltaE", ";M [GeV];deltaE [GeV];", 50, 1.68, 1.86, 50, -0.3, 0.2, "M_deltaE_before_cut.png");
+    //loader.DrawTH2D("(E*E-px*px-py*py-pz*pz)^0.5", "deltaE", ";M [GeV];deltaE [GeV];", 50, 1.681298, 1.8656, 50, -0.296904, 0.20147, "M_deltaE_before_cut.png");
 
     loader.Cut("(0.3 < daughter__bo0__cm__sppx__bc) && (0.6 < daughter__bo0__cmmuonID__bc)");
     loader.Cut("(0.3 < daughter__bo1__cm__sppx__bc) && (0.6 < daughter__bo1__cmmuonID__bc)");
     loader.Cut("(0.3 < daughter__bo2__cm__sppx__bc) && (0.6 < daughter__bo2__cmmuonID__bc)");
-    loader.PrintInformation("========== muon p > 0.3 && muonID > 0.6 ==========");
+    loader.PrintInformation("========== 0.3 < p_muon && 0.6 < muonID ==========");
 
-    loader.DrawTH1D("(E*E-px*px-py*py-pz*pz)^0.5", "M_after_cut", "M_after_cut.png");
-    loader.DrawTH1D("deltaE", "deltaE_after_cut", "deltaE_after_cut.png");
-    loader.DrawTH2D("(E*E-px*px-py*py-pz*pz)^0.5", "deltaE", ";M [GeV];deltaE [GeV];", 50, 1.68, 1.86, 50, -0.3, 0.2, "M_deltaE_after_cut.png");
+    //loader.DrawTH1D("(E*E-px*px-py*py-pz*pz)^0.5", "M_after_cut", "M_after_cut.png");
+    //loader.DrawTH1D("deltaE", "deltaE_after_cut", "deltaE_after_cut.png");
+    //loader.DrawTH2D("(E*E-px*px-py*py-pz*pz)^0.5", "deltaE", ";M [GeV];deltaE [GeV];", 50, 1.681298, 1.8656, 50, -0.296904, 0.20147, "M_deltaE_after_cut.png");
+
+    //loader.DrawTH1D("missingMomentumOfEventCMS_theta", "theta_miss", "theta_miss.png");
+    loader.Cut("(0.3 < missingMomentumOfEventCMS_theta) && (missingMomentumOfEventCMS_theta < 2.7)");
+    loader.PrintInformation("========== 0.3 < theta_miss < 2.7 ==========");
+
+    loader.Cut("(0.89 < thrust) && (thrust < 0.97)");
+    loader.PrintInformation("========== 0.89 < thrust < 0.97 ==========");
 
     loader.PrintSeparateRootFile("./", "", "_after_cut");
 
