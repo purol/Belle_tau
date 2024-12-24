e420f5aa8a17d0ec9f80601658651af42c77f1f1
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-12-24 23:44:15 +0900

    separete analyzer and logger

--- analysis_code/src/Analysis_main_log.cc
+++ analysis_code/src/Analysis_main_log.cc
@@ -53,15 +53,13 @@ double MyScaleFunction(std::vector<Data>::iterator data_) {
 int main(int argc, char* argv[]) {
     /*
     * argv[1]: dirname
-    * argv[2]: filename
-    * argv[3]: output path
     */
 
     ObtainWeight = MyScaleFunction;
 
     Loader loader("tau_lfv");
 
-    loader.Load(argv[1], argv[2], "label");
+    loader.Load(argv[1], ".root", "label");
     loader.Cut("(0.5 < extraInfo__bodecayModeID__bc) && (extraInfo__bodecayModeID__bc < 1.5)"); // select tau -> mu mu mu only
 
     loader.PrintInformation("========== initial ==========");
@@ -74,7 +72,7 @@ int main(int argc, char* argv[]) {
 
     //loader.DrawTH2D("(E*E-px*px-py*py-pz*pz)^0.5", "deltaE", ";M [GeV];deltaE [GeV];", 50, 1.681298, 1.8656, 50, -0.296904, 0.20147, "M_deltaE_before_cut.png");
 
-    loader.PrintSeparateRootFile((std::string(argv[3]) + "/before_muon_selection").c_str(), "", "");
+    //loader.PrintSeparateRootFile((std::string(argv[2]) + "/before_muon_selection").c_str(), "", "");
 
     //loader.Cut("(0.3 < daughter__bo0__cm__sppx__bc) && (0.6 < daughter__bo0__cmmuonID__bc)");
     loader.Cut("(0.3 < daughter__bo1__cm__sppx__bc) && (0.6 < daughter__bo1__cmmuonID__bc)");
@@ -85,19 +83,19 @@ int main(int argc, char* argv[]) {
     //loader.DrawTH1D("deltaE", "deltaE_after_cut", "deltaE_after_cut.png");
     //loader.DrawTH2D("(E*E-px*px-py*py-pz*pz)^0.5", "deltaE", ";M [GeV];deltaE [GeV];", 50, 1.681298, 1.8656, 50, -0.296904, 0.20147, "M_deltaE_after_cut.png");
 
-    loader.PrintSeparateRootFile((std::string(argv[3]) + "/before_theta_miss_cut").c_str(), "", "");
+    //loader.PrintSeparateRootFile((std::string(argv[2]) + "/before_theta_miss_cut").c_str(), "", "");
 
     //loader.DrawTH1D("missingMomentumOfEventCMS_theta", "theta_miss", "theta_miss.png");
     loader.Cut("(0.3 < missingMomentumOfEventCMS_theta) && (missingMomentumOfEventCMS_theta < 2.7)");
     loader.PrintInformation("========== 0.3 < theta_miss < 2.7 ==========");
 
-    loader.PrintSeparateRootFile((std::string(argv[3]) + "/before_thrust_cut").c_str(), "", "");
+    //loader.PrintSeparateRootFile((std::string(argv[2]) + "/before_thrust_cut").c_str(), "", "");
 
     //loader.DrawTH1D("thrust", "thrust", "thrust.png");
     loader.Cut("(0.89 < thrust) && (thrust < 0.97)");
     loader.PrintInformation("========== 0.89 < thrust < 0.97 ==========");
 
-    loader.PrintSeparateRootFile((std::string(argv[3]) + "/final_output").c_str(), "", "");
+    //loader.PrintSeparateRootFile((std::string(argv[2]) + "/final_output").c_str(), "", "");
 
     loader.end();
 
