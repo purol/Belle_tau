add32ff45a97fd61d9486d1c929d5bda6a80444b
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-12-24 14:17:08 +0900

    bug fix

--- analysis_code/src/Analysis_main.cc
+++ analysis_code/src/Analysis_main.cc
@@ -16,30 +16,30 @@
 # define Nevt_SIGNAL (Nevt_taupair * BR_SIGNAL * 2.0)
 # define Scale_SIGNAL_MC15ri (Nevt_SIGNAL/Nevt_SIGNAL_MC15ri)
 
-double reserve_function(std::vector<Data>::iterator data_) {
-    if (data_.find("CHG_") != std::string::npos) return Scale_CHG_MC15ri;
-    else if (data_.find("MIX_") != std::string::npos) return Scale_MIX_MC15ri;
-    else if (data_.find("UUBAR_") != std::string::npos) return Scale_UUBAR_MC15ri;
-    else if (data_.find("DDBAR_") != std::string::npos) return Scale_DDBAR_MC15ri;
-    else if (data_.find("SSBAR_") != std::string::npos) return Scale_SSBAR_MC15ri;
-    else if (data_.find("CHARM_") != std::string::npos) return Scale_CHARM_MC15ri;
-    else if (data_.find("MUMU_") != std::string::npos) return Scale_MUMU_MC15ri;
-    else if (data_.find("EE_") != std::string::npos) return Scale_EE_MC15ri;
-    else if (data_.find("EEEE_") != std::string::npos) return Scale_EEEE_MC15ri;
-    else if (data_.find("EEMUMU_") != std::string::npos) return Scale_EEMUMU_MC15ri;
-    else if (data_.find("EEPIPI_") != std::string::npos) return Scale_EEPIPI_MC15ri;
-    else if (data_.find("EEKK_") != std::string::npos) return Scale_EEKK_MC15ri;
-    else if (data_.find("EEPP_") != std::string::npos) return Scale_EEPP_MC15ri;
-    else if (data_.find("PIPIISR_") != std::string::npos) return Scale_PIPIISR_MC15ri;
-    else if (data_.find("KKISR_") != std::string::npos) return Scale_KKISR_MC15ri;
-    else if (data_.find("GG_") != std::string::npos) return Scale_GG_MC15ri;
-    else if (data_.find("EETAUTAU_") != std::string::npos) return Scale_EETAUTAU_MC15ri;
-    else if (data_.find("K0K0BARISR_") != std::string::npos) return Scale_K0K0BARISR_MC15ri;
-    else if (data_.find("MUMUMUMU_") != std::string::npos) return Scale_MUMUMUMU_MC15ri;
-    else if (data_.find("MUMUTAUTAU_") != std::string::npos) return Scale_MUMUTAUTAU_MC15ri;
-    else if (data_.find("TAUTAUTAUTAU_") != std::string::npos) return Scale_TAUTAUTAUTAU_MC15ri;
-    else if (data_.find("TAUPAIR_") != std::string::npos) return Scale_TAUPAIR_MC15ri;
-    else if (data_.find("SIGNAL_") != std::string::npos) return Scale_SIGNAL_MC15ri;
+double MyScaleFunction(std::vector<Data>::iterator data_) {
+    if (data_.filename.find("CHG_") != std::string::npos) return Scale_CHG_MC15ri;
+    else if (data_.filename.find("MIX_") != std::string::npos) return Scale_MIX_MC15ri;
+    else if (data_.filename.find("UUBAR_") != std::string::npos) return Scale_UUBAR_MC15ri;
+    else if (data_.filename.find("DDBAR_") != std::string::npos) return Scale_DDBAR_MC15ri;
+    else if (data_.filename.find("SSBAR_") != std::string::npos) return Scale_SSBAR_MC15ri;
+    else if (data_.filename.find("CHARM_") != std::string::npos) return Scale_CHARM_MC15ri;
+    else if (data_.filename.find("MUMU_") != std::string::npos) return Scale_MUMU_MC15ri;
+    else if (data_.filename.find("EE_") != std::string::npos) return Scale_EE_MC15ri;
+    else if (data_.filename.find("EEEE_") != std::string::npos) return Scale_EEEE_MC15ri;
+    else if (data_.filename.find("EEMUMU_") != std::string::npos) return Scale_EEMUMU_MC15ri;
+    else if (data_.filename.find("EEPIPI_") != std::string::npos) return Scale_EEPIPI_MC15ri;
+    else if (data_.filename.find("EEKK_") != std::string::npos) return Scale_EEKK_MC15ri;
+    else if (data_.filename.find("EEPP_") != std::string::npos) return Scale_EEPP_MC15ri;
+    else if (data_.filename.find("PIPIISR_") != std::string::npos) return Scale_PIPIISR_MC15ri;
+    else if (data_.filename.find("KKISR_") != std::string::npos) return Scale_KKISR_MC15ri;
+    else if (data_.filename.find("GG_") != std::string::npos) return Scale_GG_MC15ri;
+    else if (data_.filename.find("EETAUTAU_") != std::string::npos) return Scale_EETAUTAU_MC15ri;
+    else if (data_.filename.find("K0K0BARISR_") != std::string::npos) return Scale_K0K0BARISR_MC15ri;
+    else if (data_.filename.find("MUMUMUMU_") != std::string::npos) return Scale_MUMUMUMU_MC15ri;
+    else if (data_.filename.find("MUMUTAUTAU_") != std::string::npos) return Scale_MUMUTAUTAU_MC15ri;
+    else if (data_.filename.find("TAUTAUTAUTAU_") != std::string::npos) return Scale_TAUTAUTAUTAU_MC15ri;
+    else if (data_.filename.find("TAUPAIR_") != std::string::npos) return Scale_TAUPAIR_MC15ri;
+    else if (data_.filename.find("SIGNAL_") != std::string::npos) return Scale_SIGNAL_MC15ri;
     else {
         printf("unexpected sample type\n");
         exit(1);
@@ -56,7 +56,7 @@ int main(int argc, char* argv[]) {
     * argv[2]: output path
     */
 
-    ObtainWeight = reserve_function;
+    ObtainWeight = MyScaleFunction;
 
     Loader loader("tau_lfv");
 
