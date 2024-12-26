e20c3142f3e4a9410aae2065a06242aedce0b961
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-12-26 23:14:10 +0900

    update

--- analysis_code/src/File_Splitter.cc
+++ analysis_code/src/File_Splitter.cc
@@ -53,20 +53,21 @@ double MyScaleFunction(std::vector<Data>::iterator data_) {
 int main(int argc, char* argv[]) {
     /*
     * argv[1]: dirname
-    * argv[2]: split_num
-    * argv[3]: selected_index
-    * argv[4]: output path
+    * argv[2]: filename
+    * argv[3]: split_num
+    * argv[4]: selected_index
+    * argv[5]: output path
     */
 
     ObtainWeight = MyScaleFunction;
 
     Loader loader("tau_lfv");
 
-    loader.Load(argv[1], "root", "label");
+    loader.Load(argv[1], argv[2], "label");
 
-    loader.RandomEventSelection(atoi(argv[2]), atoi(argv[3]));
+    loader.RandomEventSelection(atoi(argv[3]), atoi(argv[4]));
 
-    loader.PrintSeparateRootFile(argv[4], "", ("split_" + std::string(argv[2]) + "_" + std::string(argv[3])).c_str());
+    loader.PrintSeparateRootFile(argv[5], "", ("split_" + std::string(argv[3]) + "_" + std::string(argv[4])).c_str());
 
     loader.end();
 
