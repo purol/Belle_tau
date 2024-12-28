722b179ede2f6592a6c06535da9b59e19668d379
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-12-29 03:33:10 +0900

    fix constant

--- analysis_code/include/constants.h
+++ analysis_code/include/constants.h
@@ -28,8 +28,7 @@
 # define tau_crosssection 0.919 // nb
 # define Nevt_taupair ((0.36537/0.000000001) * tau_crosssection)
 
-// I only use `mdst_000001_prod00026883_task10020000001.root`
-# define Nevt_SIGNAL_MC15ri 552000
+# define Nevt_SIGNAL_MC15ri 5000000
 
 # define BR_SIGNAL 0.00000001 // just set 10^(-8) 
 # define Nevt_SIGNAL (Nevt_taupair * BR_SIGNAL * 2.0)
