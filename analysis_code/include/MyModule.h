76a2fd5209d3ad0ccf0c709e2f14f4a0d70c1c68
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-12-29 18:37:33 +0900

    add Mymodule

---
+++ analysis_code/include/MyModule.h
@@ -0,0 +1,238 @@
+#ifndef MYMODULE_H
+#define MYMODULE_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include <module.h>
+
+namespace Module {
+
+    class ConditionalCut : public Module {
+    private:
+        std::vector<std::string> equation_list;
+        std::vector<std::string> replaced_expr_list;
+
+        std::string condition;
+        std::string inequality;
+        double value;
+
+        std::vector<std::string>* variable_names;
+        std::vector<std::string>* VariableTypes;
+
+        static char to_upper(char c) {
+            return std::toupper(static_cast<unsigned char>(c));
+        }
+
+    public:
+        ConditionalCut(std::vector<std::string> equation_list_, const char* condition_, const char* inequality_, double value_, std::vector<std::string>* variable_names_, std::vector<std::string>* VariableTypes_) : Module(), equation_list(equation_list_), condition(condition_), inequality(inequality_), value(value_), variable_names(variable_names_), VariableTypes(VariableTypes_) {}
+        ~ConditionalCut() {}
+
+        void Start() {
+            for (int i = 0; i < equation_list.size(); i++) {
+                std::string replaced_expr = replaceVariables(equation_list.at(i), variable_names);
+                replaced_expr_list.push_back(replaced_expr);
+            }
+
+            // convert `condition` into upper case
+            std::transform(condition.begin(), condition.end(), condition.begin(), to_upper);
+
+            if ((condition != "HIGHEST") && (condition != "LOWEST")) {
+                printf("condition for ConditionalCut should be `highest` or `lowest`\n");
+                exit(1);
+            }
+
+            // check `inequality`
+            if (inequality == "<") {}
+            else if (inequality == ">") {}
+            else if (inequality == "<=") {}
+            else if (inequality == ">=") {}
+            else if (inequality == "==") {}
+            else if (inequality == "!=") {}
+            else {
+                printf("inequality for ConditionalCut should be one of `>`, `<`, `<=`, `>=`, `==`, or `!=`\n");
+                exit(1);
+            }
+        }
+
+        int Process(std::vector<Data>* data) override {
+            for (std::vector<Data>::iterator iter = data->begin(); iter != data->end(); ) {
+                double result = -1;
+                if (condition != "HIGHEST") result = std::numeric_limits<double>::lowest();
+                else result = std::numeric_limits<double>::max();
+
+                for (int i = 0; i < replaced_expr_list.size(); i++) {
+                    double temp_ = evaluateExpression(replaced_expr_list.at(i), iter->variable, VariableTypes);
+                    if ((condition != "HIGHEST") && (temp_ > result)) result = temp_;
+                    else if ((condition != "LOWEST") && (temp_ < result)) result = temp_;
+                }
+
+                // then consider about (in)equality
+                if (inequality == "<") {
+                    if (result < value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+                else if (inequality == ">") {
+                    if (result > value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+                else if (inequality == "<=") {
+                    if (result <= value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+                else if (inequality == ">=") {
+                    if (result >= value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+                else if (inequality == "==") {
+                    if (result == value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+                else if (inequality == "!=") {
+                    if (result != value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+
+            }
+
+            return 1;
+        }
+
+        void End() override {}
+    };
+
+    class ConditionalPairCut : public Module {
+    private:
+        /*
+        * contition equation - criteria equation
+        * conditional euqaition is used to select criteria equation. It is determined by highest or lowest
+        * criteria equation is used to select candidates.
+        */
+        std::map<std::string, std::string> condition_equation__criteria_equation_list;
+        std::map<std::string, std::string> condition_replaced_expr__criteria_replaced_expr_list;
+
+        std::string condition;
+        std::string inequality;
+        double value;
+
+        std::vector<std::string>* variable_names;
+        std::vector<std::string>* VariableTypes;
+
+        static char to_upper(char c) {
+            return std::toupper(static_cast<unsigned char>(c));
+        }
+
+    public:
+        ConditionalPairCut(std::map<std::string, std::string> condition_equation__criteria_equation_list_, const char* condition_, const char* inequality_, double value_, std::vector<std::string>* variable_names_, std::vector<std::string>* VariableTypes_) : Module(), condition_equation__criteria_equation_list(condition_equation__criteria_equation_list_), condition(condition_), inequality(inequality_), value(value_), variable_names(variable_names_), VariableTypes(VariableTypes_) {}
+        ~ConditionalPairCut() {}
+
+        void Start() {
+            for (std::map<std::string, std::string>::iterator iter_eq = condition_equation__criteria_equation_list.begin(); iter_eq != condition_equation__criteria_equation_list.end(); ++iter_eq) {
+                std::string condition_replaced_expr = replaceVariables(iter_eq->first, variable_names);
+                std::string criteria_replaced_expr = replaceVariables(iter_eq->second, variable_names);
+
+                condition_replaced_expr__criteria_replaced_expr_list.insert(std::make_pair(condition_replaced_expr, criteria_replaced_expr));
+            }
+
+            // convert `condition` into upper case
+            std::transform(condition.begin(), condition.end(), condition.begin(), to_upper);
+
+            if ((condition != "HIGHEST") && (condition != "LOWEST")) {
+                printf("condition for ConditionalPairCut should be `highest` or `lowest`\n");
+                exit(1);
+            }
+
+            // check `inequality`
+            if (inequality == "<") {}
+            else if (inequality == ">") {}
+            else if (inequality == "<=") {}
+            else if (inequality == ">=") {}
+            else if (inequality == "==") {}
+            else if (inequality == "!=") {}
+            else {
+                printf("inequality for ConditionalPairCut should be one of `>`, `<`, `<=`, `>=`, `==`, or `!=`\n");
+                exit(1);
+            }
+        }
+
+        int Process(std::vector<Data>* data) override {
+            for (std::vector<Data>::iterator iter = data->begin(); iter != data->end(); ) {
+                double condition_result = -1;
+                double criteria_result = std::numeric_limits<double>::max();
+                if (condition != "HIGHEST") condition_result = std::numeric_limits<double>::lowest();
+                else condition_result = std::numeric_limits<double>::max();
+
+                for (std::map<std::string, std::string>::iterator iter_eq = condition_replaced_expr__criteria_replaced_expr_list.begin(); iter_eq != condition_replaced_expr__criteria_replaced_expr_list.end(); ++iter_eq) {
+                    double temp_ = evaluateExpression(iter_eq->first, iter->variable, VariableTypes);
+                    if ((condition != "HIGHEST") && (temp_ > condition_result)) {
+                        condition_result = temp_;
+                        criteria_result = iter_eq->second;
+                    }
+                    else if ((condition != "LOWEST") && (temp_ < condition_result)) {
+                        condition_result = temp_;
+                        criteria_result = iter_eq->second;
+                    }
+                }
+
+                // then consider about (in)equality
+                if (inequality == "<") {
+                    if (criteria_result < value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+                else if (inequality == ">") {
+                    if (criteria_result > value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+                else if (inequality == "<=") {
+                    if (criteria_result <= value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+                else if (inequality == ">=") {
+                    if (criteria_result >= value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+                else if (inequality == "==") {
+                    if (criteria_result == value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+                else if (inequality == "!=") {
+                    if (criteria_result != value) {
+                        ++iter;
+                    }
+                    else data->erase(iter);
+                }
+
+            }
+
+            return 1;
+        }
+
+        void End() override {}
+    };
+
+}
+
+#endif 
--- analysis_code/src/File_Splitter.cc
+++ analysis_code/src/File_Splitter.cc
@@ -6,40 +6,7 @@
 
 #include "Loader.h"
 #include "constants.h"
-
-double MyScaleFunction(std::vector<Data>::iterator data_) {
-    if ((*data_).filename.find("CHG_") != std::string::npos) return Scale_CHG_MC15ri;
-    else if ((*data_).filename.find("MIX_") != std::string::npos) return Scale_MIX_MC15ri;
-    else if ((*data_).filename.find("UUBAR_") != std::string::npos) return Scale_UUBAR_MC15ri;
-    else if ((*data_).filename.find("DDBAR_") != std::string::npos) return Scale_DDBAR_MC15ri;
-    else if ((*data_).filename.find("SSBAR_") != std::string::npos) return Scale_SSBAR_MC15ri;
-    else if ((*data_).filename.find("CHARM_") != std::string::npos) return Scale_CHARM_MC15ri;
-    else if ((*data_).filename.find("MUMU_") != std::string::npos) return Scale_MUMU_MC15ri;
-    else if ((*data_).filename.find("EE_") != std::string::npos) return Scale_EE_MC15ri;
-    else if ((*data_).filename.find("EEEE_") != std::string::npos) return Scale_EEEE_MC15ri;
-    else if ((*data_).filename.find("EEMUMU_") != std::string::npos) return Scale_EEMUMU_MC15ri;
-    else if ((*data_).filename.find("EEPIPI_") != std::string::npos) return Scale_EEPIPI_MC15ri;
-    else if ((*data_).filename.find("EEKK_") != std::string::npos) return Scale_EEKK_MC15ri;
-    else if ((*data_).filename.find("EEPP_") != std::string::npos) return Scale_EEPP_MC15ri;
-    else if ((*data_).filename.find("PIPIISR_") != std::string::npos) return Scale_PIPIISR_MC15ri;
-    else if ((*data_).filename.find("KKISR_") != std::string::npos) return Scale_KKISR_MC15ri;
-    else if ((*data_).filename.find("GG_") != std::string::npos) return Scale_GG_MC15ri;
-    else if ((*data_).filename.find("EETAUTAU_") != std::string::npos) return Scale_EETAUTAU_MC15ri;
-    else if ((*data_).filename.find("K0K0BARISR_") != std::string::npos) return Scale_K0K0BARISR_MC15ri;
-    else if ((*data_).filename.find("MUMUMUMU_") != std::string::npos) return Scale_MUMUMUMU_MC15ri;
-    else if ((*data_).filename.find("MUMUTAUTAU_") != std::string::npos) return Scale_MUMUTAUTAU_MC15ri;
-    else if ((*data_).filename.find("TAUTAUTAUTAU_") != std::string::npos) return Scale_TAUTAUTAUTAU_MC15ri;
-    else if ((*data_).filename.find("TAUPAIR_") != std::string::npos) return Scale_TAUPAIR_MC15ri;
-    else if ((*data_).filename.find("SIGNAL_") != std::string::npos) return Scale_SIGNAL_MC15ri;
-    else {
-        printf("unexpected sample type\n");
-        exit(1);
-        return 0.0;
-    }
-    printf("unexpected sample type\n");
-    exit(1);
-    return 0.0;
-}
+#include "MyObtainWeight.h"
 
 int main(int argc, char* argv[]) {
     /*
