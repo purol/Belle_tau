0ec5f30ff57b6b0f851cf1a62815f4ddb537ef0e
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-01-13 14:53:57 +0900

    add FBDT application

---
+++ analysis_code/src/FBDT_Application.cc
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "TFile.h"
+
+#include "Loader.h"
+#include "constants.h"
+#include "MyObtainWeight.h"
+#include "MyModule.h"
+
+std::string ReadSelect(const char* select_path) {
+    unsigned int nTrees;
+    unsigned int depth;
+    double shrinkage;
+    double subsample;
+    unsigned int binning;
+    double train_AUC;
+    double test_AUC;
+
+    FILE* fp = fopen((std::string(select_path) + "/selected.txt").c_str(), "r");
+    fscanf(fp, "%u_%u_%lf_%lf_%u %lf %lf\n", &nTrees, &depth, &shrinkage, &subsample, &binning, &train_AUC, &test_AUC);
+    fclose(fp);
+
+    std::string classifier_path = std::string(select_path) + "/out/" + std::to_string(nTrees) + "_" + std::to_string(depth) + "_" + std::to_string(shrinkage) + "_" + std::to_string(subsample) + "_" + std::to_string(binning) + ".weightfile";
+    return classifier_path;
+}
+
+int main(int argc, char* argv[]) {
+    /*
+    * argv[1]: variable num (ex. N in here)
+    * argv[2]: variable name
+    * argv[1 + N]: variable name
+    * argv[2 + N]: input path
+    * argv[3 + N]: output path
+    * argv[4 + N]: select.txt path
+    */
+
+    int variable_num = std::atoi(argv[1]);
+    std::vector<std::string> intput_variables;
+    for (int i = 0; i < variable_num; i++) {
+        std::string variable_(argv[2 + i]);
+        intput_variables.push_back(variable_);
+    }
+
+    std::string classifier_path = ReadSelect(argv[4 + variable_num]);
+
+    ObtainWeight = MyScaleFunction_halfsplit;
+
+    Loader loader("tau_lfv");
+
+    loader.Load(argv[2 + variable_num], "root", "label");
+
+    loader.FastBDTApplication(intput_variables, classifier_path.c_str(), "BDT_output");
+
+    loader.PrintSeparateRootFile(argv[3 + variable_num], "", "");
+
+    loader.end();
+
+    return 0;
+}
