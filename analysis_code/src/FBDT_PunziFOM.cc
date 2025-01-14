3acaa2d1219b9cc3e547538551f74e8e5c21049c
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-01-15 01:37:44 +0900

    add PunziFOM

---
+++ analysis_code/src/FBDT_PunziFOM.cc
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "TFile.h"
+
+#include "Loader.h"
+#include "constants.h"
+#include "MyObtainWeight.h"
+#include "MyModule.h"
+
+int main(int argc, char* argv[]) {
+    /*
+    * argv[1]: input path
+    */
+
+    std::vector<std::string> signal_list = { "SIGNAL" };
+    std::vector<std::string> background_list = { "CHARM", "CHG", "DDBAR", "EE", "EEEE", 
+        "EEKK", "EEMUMU", "EEPIPI", "EEPP", "EETAUTAU", "GG", 
+        "K0K0BARISR", "KKISR", "MIX", "MUMU", "MUMUMUMU", 
+        "MUMUTAUTAU", "PIPIISR", "SSBAR", "TAUPAIR", "TAUTAUTAUTAU", "UUBAR" };
+
+    ObtainWeight = MyScaleFunction_halfsplit;
+
+    Loader loader("tau_lfv");
+
+    for (int i = 0; i < signal_list.size(); i++) loader.Load((argv[1] + std::string("/") + signal_list.at(i) + std::string("/final_output_test_after_application/")).c_str(), "root", signal_list.at(i).c_str());
+    for (int i = 0; i < background_list.size(); i++) loader.Load((argv[1] + std::string("/") + background_list.at(i) + std::string("/final_output_test_after_application/")).c_str(), "root", background_list.at(i).c_str());
+
+    // Create a new vector to hold the combined elements
+    std::vector<std::string> all_label;
+    all_label.reserve(signal_list.size() + background_list.size());
+    all_label.insert(all_label.end(), signal_list.begin(), signal_list.end());
+    all_label.insert(all_label.end(), background_list.begin(), background_list.end());
+
+    loader.SetMC(all_label);
+    loader.SetData({});
+    loader.SetSignal(signal_list);
+    loader.SetBackground(background_list);
+
+    loader.DrawPunziFOM("BDT_output", 0.0, 1.0, Nevt_SIGNAL, 1.28, (std::string(argv[1])  + "/GridSearch/PunziFOM.png").c_str());
+
+    loader.end();
+
+    return 0;
+}
