465344293d48eed05a6ba2b41533ff1b48a9a004
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-01-15 02:28:54 +0900

    add 2D plotter

---
+++ analysis_code/src/Plotter_2D_half.cc
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+#include "TFile.h"
+
+#include "Loader.h"
+#include "constants.h"
+#include "MyObtainWeight.h"
+
+int main(int argc, char* argv[]) {
+    /*
+    * argv[1]: variable name 1
+    * argv[2]: variable name 2
+    * argv[3]: input path 1
+    * argv[4]: input path 2
+    * argv[5]: output path
+    * argv[6]: output name
+    * (argv[7]): min value 1
+    * (argv[8]): max value 1
+    * (argv[9]): min value 2
+    * (argv[10]): max value 2
+    */
+
+    std::string variable_name_1(argv[1]);
+    std::string variable_name_2(argv[2]);
+
+    std::vector<std::string> signal_list = { "SIGNAL" };
+    std::vector<std::string> background_list = { "CHARM", "CHG", "DDBAR", "EE", "EEEE", 
+        "EEKK", "EEMUMU", "EEPIPI", "EEPP", "EETAUTAU", "GG", 
+        "K0K0BARISR", "KKISR", "MIX", "MUMU", "MUMUMUMU", 
+        "MUMUTAUTAU", "PIPIISR", "SSBAR", "TAUPAIR", "TAUTAUTAUTAU", "UUBAR" };
+
+    ObtainWeight = MyScaleFunction_halfsplit;
+
+    Loader loader("tau_lfv");
+
+    for (int i = 0; i < signal_list.size(); i++) loader.Load((argv[3] + std::string("/") + signal_list.at(i) + std::string("/") + std::string(argv[4])).c_str(), "root", signal_list.at(i).c_str());
+    for (int i = 0; i < background_list.size(); i++) loader.Load((argv[3] + std::string("/") + background_list.at(i) + std::string("/") + std::string(argv[4])).c_str(), "root", background_list.at(i).c_str());
+
+    // Create a new vector to hold the combined elements
+    std::vector<std::string> all_label;
+    all_label.reserve(signal_list.size() + background_list.size());
+    all_label.insert(all_label.end(), signal_list.begin(), signal_list.end());
+    all_label.insert(all_label.end(), background_list.begin(), background_list.end());
+
+    loader.SetMC(all_label);
+    loader.SetData({});
+    loader.SetSignal(signal_list);
+    loader.SetBackground(background_list);
+
+    if (argc == 7) loader.DrawTH2D(variable_name_1.c_str(), variable_name_2.c_str(), (";" + variable_name_1 + ";" + variable_name_2 + ";Number of event").c_str(), (argv[5] + std::string("/") + argv[6] + ".png").c_str());
+    else if (argc == 11) loader.DrawTH2D(variable_name_1.c_str(), variable_name_2.c_str(), (";" + variable_name_1 + ";" + variable_name_2 + ";Number of event").c_str(), 50, std::stod(argv[7]), std::stod(argv[8]), 50, std::stod(argv[9]), std::stod(argv[10]), (argv[5] + std::string("/") + argv[6] + ".png").c_str());
+
+    loader.end();
+
+    return 0;
+}
