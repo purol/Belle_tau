bfa7a61c9821b73beca067dffdba05815652aa41
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-01-14 20:01:07 +0900

    plot fix

--- analysis_code/src/1D_fit_M_deltaE.cc
+++ analysis_code/src/1D_fit_M_deltaE.cc
@@ -113,8 +113,8 @@ int main(int argc, char* argv[]) {
     TCanvas* c_M = new TCanvas("canvas_M_fit", "canvas_M_fit", 800, 800);
 
     c_M->cd();
-    TPad* pad1 = new TPad("pad1", "pad1", 0.0, 0.3, 1.0, 1.0);
-    pad1->SetBottomMargin(0.05); pad1->SetLeftMargin(0.15); pad1->SetGridx(); pad1->Draw(); pad1->cd();
+    TPad* pad1_M = new TPad("pad1", "pad1", 0.0, 0.3, 1.0, 1.0);
+    pad1_M->SetBottomMargin(0.05); pad1_M->SetLeftMargin(0.15); pad1_M->SetGridx(); pad1_M->Draw(); pad1_M->cd();
     M_inv_frame->GetXaxis()->SetLabelSize(0); M_inv_frame->GetXaxis()->SetTitleSize(0);
     M_inv_frame->Draw();
     TLegend* legend_M = new TLegend(0.2, 0.75, 0.45, 0.85);
@@ -152,12 +152,40 @@ int main(int argc, char* argv[]) {
 
     // plot deltaE fit
     RooPlot* deltaE_frame = deltaE.frame(RooFit::Bins(200), RooFit::Title(" "));
-    dataset_deltaE->plotOn(deltaE_frame, RooFit::DataError(RooAbsData::SumW2));
+    dataset_deltaE->plotOn(deltaE_frame, RooFit::DataError(RooAbsData::SumW2)), RooFit::Name("signal MC");
     bifurcated_deltaE.plotOn(deltaE_frame, RooFit::LineColor(kRed), RooFit::LineStyle(kDashed), RooFit::Range("full"), RooFit::NormRange("peak"));
-    bifurcated_deltaE.plotOn(deltaE_frame, RooFit::LineColor(kBlue), RooFit::LineStyle(kSolid), RooFit::Range("peak"), RooFit::NormRange("peak"));
+    bifurcated_deltaE.plotOn(deltaE_frame, RooFit::LineColor(kBlue), RooFit::LineStyle(kSolid), RooFit::Range("peak"), RooFit::NormRange("peak"), RooFit::Name("BifurGauss"));
+
+    RooHist* pull_deltaE = deltaE_frame->pullHist("signal MC", "BifurGauss");
+    RooPlot* deltaE_frame_pull_frame = deltaE.frame(RooFit::Title(""));
+    deltaE_frame_pull_frame->addPlotable(pull_deltaE, "P");
 
     TCanvas* c_deltaE = new TCanvas("canvas_deltaE_fit", "canvas_deltaE_fit", 800, 800);
+
+    c_deltaE->cd();
+    TPad* pad1_deltaE = new TPad("pad1", "pad1", 0.0, 0.3, 1.0, 1.0);
+    pad1_deltaE->SetBottomMargin(0.05); pad1_deltaE->SetLeftMargin(0.15); pad1_deltaE->SetGridx(); pad1_deltaE->Draw(); pad1_deltaE->cd();
+    deltaE_frame->GetXaxis()->SetLabelSize(0); deltaE_frame->GetXaxis()->SetTitleSize(0);
     deltaE_frame->Draw();
+    TLegend* legend_deltaE = new TLegend(0.2, 0.75, 0.45, 0.85);
+    legend_deltaE->AddEntry("signal MC", "signal MC", "lpe");
+    legend_deltaE->AddEntry("BifurGauss", "BifurGauss", "l");
+    legend_deltaE->SetFillStyle(0); legend_deltaE->SetLineWidth(0);
+    legend_deltaE->Draw();
+    TLatex latex_deltaE;
+    latex_deltaE.SetNDC();
+    latex_deltaE.SetTextSize(0.04);
+    latex_deltaE.DrawLatex(0.2, 0.7, ("#mu = " + toStringWithPrecision(mean_deltaE_fit, 4) + " #pm " + toStringWithPrecision(mean_deltaE_fit_error, 4) + " [GeV]").c_str());
+    latex_deltaE.DrawLatex(0.2, 0.6, ("#delta^{left}_{Gauss} = " + toStringWithPrecision(sigma_left_deltaE_fit * 1000.0, 3) + " #pm " + toStringWithPrecision(sigma_left_deltaE_fit_error * 1000.0, 3) + " [MeV]").c_str());
+    latex_deltaE.DrawLatex(0.2, 0.5, ("#delta^{right}_{Gauss} = " + toStringWithPrecision(sigma_right_deltaE_fit * 1000.0, 3) + " #pm " + toStringWithPrecision(sigma_right_deltaE_fit_error * 1000.0, 3) + " [MeV]").c_str());
+
+    c_deltaE->cd();
+    TPad* pad2 = new TPad("pad2", "pad2", 0.0, 0.0, 1, 0.3);
+    pad2->SetTopMargin(0.05); pad2->SetBottomMargin(0.3); pad2->SetLeftMargin(0.15); pad2->SetGridx(); pad2->Draw(); pad2->cd();
+    deltaE_frame_pull_frame->GetXaxis()->SetLabelSize(0.1); deltaE_frame_pull_frame->GetXaxis()->SetTitleSize(0.1); deltaE_frame_pull_frame->GetYaxis()->SetTitleOffset(0.4);
+    deltaE_frame_pull_frame->GetYaxis()->SetLabelSize(0.1); deltaE_frame_pull_frame->GetYaxis()->SetTitleSize(0.1); deltaE_frame_pull_frame->GetYaxis()->SetTitle("pull"); deltaE_frame_pull_frame->SetTitle("");
+    deltaE_frame_pull_frame->Draw();
+
     c_deltaE->SaveAs((std::string(argv[2]) + "/deltaE_fit.png").c_str());
     delete c_deltaE;
 
