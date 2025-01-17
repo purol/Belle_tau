21d77b030a211729b72ddf5fac0c060b74ef2f2e
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-01-17 10:45:52 +0900

    change region for PunziFOM

--- analysis_code/src/FBDT_PunziFOM.cc
+++ analysis_code/src/FBDT_PunziFOM.cc
@@ -10,6 +10,36 @@
 #include "MyObtainWeight.h"
 #include "MyModule.h"
 
+void ReadResolution(const char* filename_, double* deltaE_peak_, double* deltaE_left_sigma_, double* deltaE_right_sigma_, double* M_peak_, double* M_left_sigma_, double* M_right_sigma_) {
+    FILE* fp = fopen(filename_, "r");
+
+    double mean_M;
+    double sigma_left_M;
+    double sigma_right_M;
+    double result_M;
+
+    double mean_deltaE;
+    double sigma_left_deltaE;
+    double sigma_right_deltaE;
+    double result_deltaE;
+
+    double theta;
+
+    fscanf(fp, "%lf %lf %lf %d\n", &mean_M, &sigma_left_M, &sigma_right_M, &result_M);
+    fscanf(fp, "%lf %lf %lf %d\n", &mean_deltaE, &sigma_left_deltaE, &sigma_right_deltaE, &result_deltaE);
+    fscanf(fp, "%lf\n", &theta);
+
+    fclose(fp);
+
+    *deltaE_peak_ = mean_deltaE;
+    *deltaE_left_sigma_ = sigma_left_deltaE;
+    *deltaE_right_sigma_ = sigma_right_deltaE;
+
+    *M_peak_ = mean_M;
+    *M_left_sigma_ = sigma_left_M;
+    *M_right_sigma_ = sigma_right_M;
+}
+
 int main(int argc, char* argv[]) {
     /*
     * argv[1]: input path
@@ -20,6 +50,15 @@ int main(int argc, char* argv[]) {
         "EEKK", "EEMUMU", "EEPIPI", "EEPP", "EETAUTAU", "GG", 
         "K0K0BARISR", "KKISR", "MIX", "MUMU", "MUMUMUMU", 
         "MUMUTAUTAU", "PIPIISR", "SSBAR", "TAUPAIR", "TAUTAUTAUTAU", "UUBAR" };
+
+    double deltaE_peak;
+    double deltaE_left_sigma;
+    double deltaE_right_sigma;
+    double M_peak;
+    double M_left_sigma;
+    double M_right_sigma;
+
+    ReadResolution(argv[1], &deltaE_peak, &deltaE_left_sigma, &deltaE_right_sigma, &M_peak, &M_left_sigma, &M_right_sigma);
 
     ObtainWeight = MyScaleFunction_halfsplit;
 
@@ -38,6 +77,11 @@ int main(int argc, char* argv[]) {
     loader.SetData({});
     loader.SetSignal(signal_list);
     loader.SetBackground(background_list);
+
+    loader.Cut(("(deltaE < " + std::to_string(deltaE_peak + 5 * deltaE_right_sigma) + ")").c_str());
+    loader.PrintInformation("========== deltaE < 5 delta ==========");
+    loader.Cut(("(" + std::to_string(M_peak - 5 * M_left_sigma) + "< M_inv_tau) && (M_inv_tau < " + std::to_string(M_peak + 5 * M_right_sigma) + ")").c_str());
+    loader.PrintInformation("========== -5 delta < M < 5 delta ==========");
 
     loader.DrawPunziFOM("BDT_output", 0.0, 1.0, Nevt_SIGNAL, 1.28, (std::string(argv[1])  + "/GridSearch/PunziFOM.png").c_str());
 
