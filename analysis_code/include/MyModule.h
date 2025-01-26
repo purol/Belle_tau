27e9932c5ec2107283d12c4f2085772b63cf4b0e
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-01-26 21:13:00 +0900

    add FillCustomizedTH2D

--- analysis_code/include/MyModule.h
+++ analysis_code/include/MyModule.h
@@ -12,6 +12,8 @@
 #include "RooRealVar.h"
 #include "RooArgSet.h"
 #include "TProfile.h"
+#include "TH1.h"
+#include "TH2.h"
 
 namespace Module {
 
@@ -94,6 +96,149 @@ namespace Module {
                 double result_y = evaluateExpression(replaced_expr_y, iter->variable, &VariableTypes);
 
                 tprofile->Fill(result_x, result_y, ObtainWeight(iter));
+
+                ++iter;
+            }
+            return 1;
+        }
+        void End() override {}
+    };
+
+    class FillTH1D : public Module {
+        /*
+        * This module is used to fill TH1D
+        */
+    private:
+
+        TH1D* th1d;
+
+        std::string equation;
+        std::string replaced_expr;
+
+        std::vector<std::string> variable_names;
+        std::vector<std::string> VariableTypes;
+
+    public:
+        FillTH1D(TH1D* th1d_, std::string equation_, std::vector<std::string>* variable_names_, std::vector<std::string>* VariableTypes_) : Module(), th1d(th1d_), equation(equation_), variable_names(*variable_names_), VariableTypes(*VariableTypes_) {}
+        ~FillTH1D() {}
+        void Start() {
+            replaced_expr = replaceVariables(equation, &variable_names);
+        }
+        int Process(std::vector<Data>* data) override {
+            for (std::vector<Data>::iterator iter = data->begin(); iter != data->end(); ) {
+                double result = evaluateExpression(replaced_expr, iter->variable, &VariableTypes);
+
+                th1d->Fill(result, ObtainWeight(iter));
+
+                ++iter;
+            }
+            return 1;
+        }
+        void End() override {}
+    };
+
+    class FillCustomizedTH1D : public Module {
+        /*
+        * This module is used to fill TH1D with customized function
+        */
+    private:
+
+        TH1D* th1d;
+        double (*custom_function)(double);
+
+        std::string equation;
+        std::string replaced_expr;
+
+        std::vector<std::string> variable_names;
+        std::vector<std::string> VariableTypes;
+
+    public:
+        FillCustomizedTH1D(TH1D* th1d_, std::string equation_, double (*custom_function_)(double), std::vector<std::string>* variable_names_, std::vector<std::string>* VariableTypes_) : Module(), th1d(th1d_), equation(equation_), custom_function(custom_function_), variable_names(*variable_names_), VariableTypes(*VariableTypes_) {}
+        ~FillCustomizedTH1D() {}
+        void Start() {
+            replaced_expr = replaceVariables(equation, &variable_names);
+        }
+        int Process(std::vector<Data>* data) override {
+            for (std::vector<Data>::iterator iter = data->begin(); iter != data->end(); ) {
+                double result = evaluateExpression(replaced_expr, iter->variable, &VariableTypes);
+
+                th1d->Fill(custom_function(result), ObtainWeight(iter));
+
+                ++iter;
+            }
+            return 1;
+        }
+        void End() override {}
+    };
+
+    class FillTH2D : public Module {
+        /*
+        * This module is used to fill TH2D
+        */
+    private:
+
+        TH2D* th2d;
+
+        std::string x_expression;
+        std::string x_replaced_expr;
+        std::string y_expression;
+        std::string y_replaced_expr;
+
+        std::vector<std::string> variable_names;
+        std::vector<std::string> VariableTypes;
+
+    public:
+        FillTH2D(TH2D* th2d_, const char* x_expression_, const char* y_expression_, std::vector<std::string>* variable_names_, std::vector<std::string>* VariableTypes_) : Module(), th2d(th2d_), x_expression(x_expression_), y_expression(y_expression_), variable_names(*variable_names_), VariableTypes(*VariableTypes_) {}
+        ~FillTH2D() {}
+        void Start() {
+            x_replaced_expr = replaceVariables(x_expression, &variable_names);
+            y_replaced_expr = replaceVariables(y_expression, &variable_names);
+        }
+        int Process(std::vector<Data>* data) override {
+            for (std::vector<Data>::iterator iter = data->begin(); iter != data->end(); ) {
+                double x_result = evaluateExpression(x_replaced_expr, iter->variable, &VariableTypes);
+                double y_result = evaluateExpression(y_replaced_expr, iter->variable, &VariableTypes);
+
+                th1d->Fill(x_result, y_result, ObtainWeight(iter));
+
+                ++iter;
+            }
+            return 1;
+        }
+        void End() override {}
+    };
+
+    class FillCustomizedTH2D : public Module {
+        /*
+        * This module is used to fill TH2D with customized function
+        */
+    private:
+
+        TH2D* th2d;
+        double (*x_custom_function)(double, double);
+        double (*y_custom_function)(double, double);
+
+        std::string x_expression;
+        std::string x_replaced_expr;
+        std::string y_expression;
+        std::string y_replaced_expr;
+
+        std::vector<std::string> variable_names;
+        std::vector<std::string> VariableTypes;
+
+    public:
+        FillCustomizedTH2D(TH2D* th2d_, const char* x_expression_, const char* y_expression_, double (*x_custom_function_)(double, double), double (*y_custom_function_)(double, double), std::vector<std::string>* variable_names_, std::vector<std::string>* VariableTypes_) : Module(), th2d(th2d_), x_expression(x_expression_), y_expression(y_expression_), x_custom_function(x_custom_function_), y_custom_function(y_custom_function_), variable_names(*variable_names_), VariableTypes(*VariableTypes_) {}
+        ~FillCustomizedTH2D() {}
+        void Start() {
+            x_replaced_expr = replaceVariables(x_expression, &variable_names);
+            y_replaced_expr = replaceVariables(y_expression, &variable_names);
+        }
+        int Process(std::vector<Data>* data) override {
+            for (std::vector<Data>::iterator iter = data->begin(); iter != data->end(); ) {
+                double x_result = evaluateExpression(x_replaced_expr, iter->variable, &VariableTypes);
+                double y_result = evaluateExpression(y_replaced_expr, iter->variable, &VariableTypes);
+
+                th1d->Fill(x_custom_function(x_result, y_result), y_custom_function(x_result, y_result), ObtainWeight(iter));
 
                 ++iter;
             }
---
+++ analysis_code/src/CreateWorkSpace.cc
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+#include "TFile.h"
+
+#include "Loader.h"
+#include "constants.h"
+#include "MyObtainWeight.h"
+#include "functions.h"
+#include "MyModule.h"
+
+int main(int argc, char* argv[]) {
+    /*
+    * argv[1]: input path 1
+    * argv[2]: input path 2
+    */
+
+    std::vector<std::string> signal_list = { "SIGNAL" };
+    std::vector<std::string> background_list = { "CHARM", "CHG", "DDBAR", "EE", "EEEE", 
+        "EEKK", "EEMUMU", "EEPIPI", "EEPP", "EETAUTAU", "GG", 
+        "K0K0BARISR", "KKISR", "MIX", "MUMU", "MUMUMUMU", 
+        "MUMUTAUTAU", "PIPIISR", "SSBAR", "TAUPAIR", "TAUTAUTAUTAU", "UUBAR" };
+
+    double deltaE_peak;
+    double deltaE_left_sigma;
+    double deltaE_right_sigma;
+    double M_peak;
+    double M_left_sigma;
+    double M_right_sigma;
+    double theta;
+
+    ReadResolution((std::string(argv[1]) + "/M_deltaE_result.txt").c_str(), &deltaE_peak, &deltaE_left_sigma, &deltaE_right_sigma, &M_peak, &M_left_sigma, &M_right_sigma, &theta);
+
+    ObtainWeight = MyScaleFunction_halfsplit;
+
+    Loader loader("tau_lfv");
+
+    for (int i = 0; i < signal_list.size(); i++) loader.Load((argv[1] + std::string("/") + signal_list.at(i) + std::string("/") + std::string(argv[2])).c_str(), "root", signal_list.at(i).c_str());
+    for (int i = 0; i < background_list.size(); i++) loader.Load((argv[1] + std::string("/") + background_list.at(i) + std::string("/") + std::string(argv[2])).c_str(), "root", background_list.at(i).c_str());
+
+    // Create a new vector to hold the combined elements
+    std::vector<std::string> all_label;
+    all_label.reserve(signal_list.size() + background_list.size());
+    all_label.insert(all_label.end(), signal_list.begin(), signal_list.end());
+    all_label.insert(all_label.end(), background_list.begin(), background_list.end());
+
+    loader.SetMC(all_label);
+    loader.SetData({});
+    loader.SetSignal(signal_list);
+    loader.SetBackground(background_list);
+
+    loader.end();
+
+    return 0;
+}
