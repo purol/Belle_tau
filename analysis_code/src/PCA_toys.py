f7b090b2b619c9706663cea3f3eba7959f325f5b
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-02-27 16:21:16 +0900

    specify path

--- analysis_code/src/PCA_toys.py
+++ analysis_code/src/PCA_toys.py
@@ -6,6 +6,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
+import argparse
+
 # Function to plot and save correlation matrices
 def save_correlation_matrix(corr_matrix, title, filename):
     plt.figure(figsize=(6, 5))
@@ -17,10 +19,15 @@ def save_correlation_matrix(corr_matrix, title, filename):
 def covariance_to_correlation(cov_matrix):
     std_devs = np.sqrt(np.diag(cov_matrix))  # Get standard deviations
     correlation_matrix = cov_matrix / np.outer(std_devs, std_devs)  # Normalize
-    return correlation_matrix    
+    return correlation_matrix
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_file', required=True, type=str, help='input path + input file name')
+parser.add_argument('--output_file', required=True, type=str, help='output path + output file name')
+args = parser.parse_args()
 
 # Load CSV file
-df = pd.read_csv("muonID_toys.csv", header=None)  # No header in your data
+df = pd.read_csv(args.input_file, header=None)  # No header in your data
 num_samples, num_features = df.shape
 
 # Compute correlation matrix before PCA
@@ -54,8 +61,8 @@ cov_after = eigenvectors_selected.T @ Lambda_selected @ eigenvectors_selected
 corr_after = covariance_to_correlation(cov_after)
 
 # Save correlation matrices as PNG
-save_correlation_matrix(corr_before, "Correlation Matrix Before PCA", "corr_before_pca.png")
-save_correlation_matrix(corr_after, "Correlation Matrix After PCA", "corr_after_pca.png")
+save_correlation_matrix(corr_before, "Correlation Matrix Before PCA", args.output_file + "_corr_before_pca.png")
+save_correlation_matrix(corr_after, "Correlation Matrix After PCA", args.output_file + "_corr_after_pca.png")
 
 # Print explained variance ratio
 print("Explained Variance Ratio:", pca.explained_variance_ratio_)
@@ -66,7 +73,7 @@ print(eigenvalues_selected)
 print(eigenvectors_selected)
 
 # save in file
-with open('PID_calculator_result.txt', "w") as file:
+with open(args.output_file, "w") as file:
     file.write("%d,%d\n" % (num_features, dim))
     for i in range(dim):
         file.write("%f\n" % eigenvalues_selected[i])
