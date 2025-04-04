421e9199d9c19ceb4603a900b2d6b5fba57689d7
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-04-04 21:17:13 +0900

    add decay investigator

---
+++ analysis_code/src/DecayInvestigator.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import root_pandas
+import decayHash
+import basf2 as b2
+from decayHash import DecayHashMap
+import sys
+import os
+import glob
+import gc
+import re
+
+root_path = sys.argv[1] # path which includes named root file
+hashmap_path = sys.argv[2] # path which includes hashmap
+
+output_path = sys.argv[3]
+output_file_name = sys.argv[4]
+
+root_list = [f for f in os.listdir(root_path) if f.endswith('.root')]
+f = open(output_path + "/" + output_file_name, 'w')
+
+for i in range(0, len(root_list)):
+
+    try:
+        data = root_pandas.read_root(os.path.join(root_path, root_list[i]),"tau_lfv")
+    except Exception as e:
+        continue
+
+    if len(data) == 0:
+        continue
+
+    # extract job id
+    match = re.search(r'job(\d+)', file_list[i])
+    if match:
+        job_number = match.group(1)
+    else:
+        print("No job number found.")
+        sys.exit(0)
+
+    hashfile = glob.glob(hashmap_path+"*"+str(job_number)+ "*.root")
+    hashfile = [f for f in hashfile if "vertex" not in f]
+    hashfile = hashfile[0]
+    
+    hashmap = DecayHashMap(hashfile, removeRadiativeGammaFlag=True)
+
+    for j in range(0, len(data)):
+
+        candidate = data.iloc[j][["extraInfo__boDecayHash__bc", "extraInfo__boDecayHashExtended__bc"]].values
+        f.write(str(data.iloc[j]["__experiment__"]) + "\n")
+        f.write(str(data.iloc[j]["__run__"]) + "\n")
+        f.write(str(data.iloc[j]["__event__"]) + "\n")
+        f.write(str(data.iloc[j]["__candidate__"]) + "\n")
+        f.write(str(data.iloc[j]["__ncandidates__"]) + "\n")
+
+        # print the reconstruced decay
+        f.write("Reconstructed Decay: " + "\n")
+        rec = hashmap.get_reconstructed_decay(*candidate)
+        f.write(rec.to_string() + "\n")
+
+        # print the original decay as simulated in MC
+        f.write("Monte Carlo Decay: " + "\n")
+        org = hashmap.get_original_decay(*candidate)
+        f.write(org.to_string() + "\n")
+
+        f.write("===============================" + "\n")
+        f.flush()
+
+    del [[data]]
+    gc.collect()
+    
+f.close()
+sys.exit(0)
