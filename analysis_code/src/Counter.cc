5f90c00e55fe537fed07517f1d25004087851d77
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-04-15 16:51:52 +0900

    fix counter

--- analysis_code/src/Counter.cc
+++ analysis_code/src/Counter.cc
@@ -30,17 +30,26 @@ int main(int argc, char* argv[]) {
 
     ObtainWeight = MyScaleFunction_halfsplit;
 
-    Loader loader("tau_lfv");
+    Loader loader_one("tau_lfv");
+    loader_one.Load(argv[1], argv[2], "label");
+    loader_one.PrintInformation("========== initial ==========");
+    loader_one.Cut(("(" + std::to_string(deltaE_peak - 5 * deltaE_left_sigma) + "< deltaE) && (deltaE < " + std::to_string(deltaE_peak + 5 * deltaE_right_sigma) + ")").c_str());
+    loader_one.PrintInformation("========== -5 delta < deltaE < 5 delta ==========");
+    loader_one.Cut(("(" + std::to_string(M_peak - 5 * M_left_sigma) + "< M_inv_tau) && (M_inv_tau < " + std::to_string(M_peak + 5 * M_right_sigma) + ")").c_str());
+    loader_one.PrintInformation("========== -5 delta < M < 5 delta ==========");
+    loader_one.end();
 
-    loader.Load(argv[1], argv[2], "label");
-
-    loader.PrintInformation("========== initial ==========");
-    loader.Cut(("(" + std::to_string(deltaE_peak - 5 * deltaE_left_sigma) + "< deltaE) && (deltaE < " + std::to_string(deltaE_peak + 5 * deltaE_right_sigma) + ")").c_str());
-    loader.PrintInformation("========== -5 delta < deltaE < 5 delta ==========");
-    loader.Cut(("(" + std::to_string(M_peak - 5 * M_left_sigma) + "< M_inv_tau) && (M_inv_tau < " + std::to_string(M_peak + 5 * M_right_sigma) + ")").c_str());
-    loader.PrintInformation("========== -5 delta < M < 5 delta ==========");
-
-    loader.end();
+    Loader loader_two("tau_lfv");
+    loader_two.Load(argv[1], argv[2], "label");
+    loader_two.PrintInformation("========== initial ==========");
+    double sloop_1 = 10 * deltaE_left_sigma / M_right_sigma;
+    double const_term_1 = -M_peak * (10 * deltaE_left_sigma / M_right_sigma) + deltaE_peak - 35 * deltaE_left_sigma;
+    double sloop_2 = -10 * deltaE_left_sigma / M_left_sigma;
+    double const_term_2 = M_peak * (10 * deltaE_left_sigma / M_left_sigma) + deltaE_peak - 35 * deltaE_left_sigma;
+    loader_two.Cut(("deltaE > ((" + std::to_string(sloop_1) + ")*M_inv_tau+(" + std::to_string(const_term_1) + "))").c_str());
+    loader_two.Cut(("deltaE > ((" + std::to_string(sloop_2) + ")*M_inv_tau+(" + std::to_string(const_term_2) + "))").c_str());
+    loader_two.PrintInformation("========== triangle region ==========");
+    loader_two.end();
 
     return 0;
 }
