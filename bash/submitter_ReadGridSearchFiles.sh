478af02bacdfb9ac002e9a59d4a2b44d95085a52
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-08-22 17:24:13 +0900

    remove unneeded bash

--- bash/submitter_ReadGridSearchFiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-code="${Belle_tau_DIR}/analysis_code/bin/ReadGridSearchFile"
-
-bsub -q s -J GRIDFILE -o "/dev/null" ${code} "./${Analysis_Name}/${Analysis_VerName}/GridSearch_one/out/" "./${Analysis_Name}/${Analysis_VerName}/GridSearch_one/"
-bsub -q s -J GRIDFILE -o "/dev/null" ${code} "./${Analysis_Name}/${Analysis_VerName}/GridSearch_two/out/" "./${Analysis_Name}/${Analysis_VerName}/GridSearch_two/"
-
