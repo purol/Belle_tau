110b7f202fc6f040e2102e8146a04bbff3bc1412
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-08-31 18:17:46 +0900

    bug fix

--- python/tau_mumumu_SKIM.py
+++ python/tau_mumumu_SKIM.py
@@ -31,16 +31,10 @@ ma.cutAndCopyList("pi+:taulfv", "pi+:all", trackCuts, path=my_path)
 ma.reconstructDecay(decayString="tau+:LFV_lll -> mu+:all mu+:all mu-:all", cut="[nParticlesInList(pi+:taulfv) < 7] and [1.4 < M < 2.0] and [-1.0 < deltaE < 0.5]", path=my_path)
 
 # muonID > 0.1 for primary muon
-ma.buildRestOfEvent("tau+:LFV_lll",path=my_path)
-roe_path = basf2.Path()
-deadEndPath = basf2.Path()
-ma.signalSideParticleFilter('tau+:LFV_lll', '', roe_path, deadEndPath)
-ma.fillSignalSideParticleList('mu+:sig', 'tau+:LFV_lll -> ^mu+:all ^mu+:all ^mu-:all', roe_path)
-ma.rankByHighest(particleList="mu+:sig", variable="p", outputVariable="p_rank",path=roe_path)
-ma.cutAndCopyList("mu+:sig_primary", "mu+:sig", cut="extraInfo(p_rank)==1", path=roe_path)
-ma.variableToSignalSideExtraInfo('mu+:sig_primary', {'muonID': 'muonID_primary_daughter'}, path=roe_path)
-my_path.for_each('RestOfEvent', 'RestOfEvents', roe_path)
-ma.applyCuts('tau+:LFV_lll', 'extraInfo(muonID_primary_daughter) > 0.1', my_path)
+Condition_one = '[[daughter(0, p) > daughter(1, p)] && [daughter(0, p) > daughter(2, p)] && [daughter(0, muonID) > 0.1]]'
+Condition_two = '[[daughter(1, p) > daughter(0, p)] && [daughter(1, p) > daughter(2, p)] && [daughter(1, muonID) > 0.1]]'
+Condition_three = '[[daughter(2, p) > daughter(0, p)] && [daughter(2, p) > daughter(1, p)] && [daughter(2, muonID) > 0.1]]'
+ma.applyCuts('tau+:LFV_lll', Condition_one + '&&' + Condition_two + '&&' + Condition_three, my_path)
 
 # event cut
 ma.applyEventCuts("nParticlesInList(tau+:LFV_lll) > 0", path=my_path)
