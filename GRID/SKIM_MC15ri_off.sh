48f3ae9ab9ec072a390a82b4f3adffc46cfbd759
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-09-05 16:21:36 +0900

    update

---
+++ GRID/SKIM_MC15ri_off.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# bkgs
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p UUBAR_SKIM_OF_1 -i /belle/collection/MC/MC15ri_uubar_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p DDBAR_SKIM_OF_1 -i /belle/collection/MC/MC15ri_ddbar_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p SSBAR_SKIM_OF_1 -i /belle/collection/MC/MC15ri_ssbar_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p CCBAR_SKIM_OF_1 -i /belle/collection/MC/MC15ri_ccbar_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p TAUTAU_SKIM_OF_1 -i /belle/collection/MC/MC15ri_taupair_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p EE_SKIM_OF_1 -i /belle/collection/MC/MC15ri_ee_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p EEEE_SKIM_OF_1 -i /belle/collection/MC/MC15ri_eeee_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p EEMUMU_SKIM_OF_1 -i /belle/collection/MC/MC15ri_eemumu_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p EETAUTAU_SKIM_OF_1 -i /belle/collection/MC/MC15ri_eetautau_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p EEPIPI_SKIM_OF_1 -i /belle/collection/MC/MC15ri_eepipi_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p EEKK_SKIM_OF_1 -i /belle/collection/MC/MC15ri_eeKK_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p EEPP_SKIM_OF_1 -i /belle/collection/MC/MC15ri_eepp_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p GG_SKIM_OF_1 -i /belle/collection/MC/MC15ri_gg_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p MUMU_SKIM_OF_1 -i /belle/collection/MC/MC15ri_mumu_4S_offres_v1 -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p MUMUMUMU_SKIM_OF_1 -i /belle/collection/MC/MC15ri_mumumumu_4S_offres_v1 -s light-2505-deimos --cputime 800
+
+# signal
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p SIGNAL_N_SKIM_OF_1 -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00036273/s00/e1003/4S_offres/r00000/3410931052/mdst -s light-2505-deimos --cputime 800
+gbasf2 ./python/tau_mumumu_SKIM.py --force -p SIGNAL_P_SKIM_OF_1 -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00036274/s00/e1003/4S_offres/r00000/3410931053/mdst -s light-2505-deimos --cputime 800
