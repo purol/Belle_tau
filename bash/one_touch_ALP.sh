af7d62b492509d7a06d018ea37822bcc8542075b
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-01-18 15:48:45 +0900

    add simple ALP analysis

---
+++ bash/one_touch_ALP.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+export Analysis_Name="Okina"
+export Analysis_VerName="v004"
+
+export Belle_tau_DIR="/home/belle2/junewoo/storage_b2/tau_workspace/Belle_tau"
+export Ntuple_DIR="/home/belle2/junewoo/storage_ghi/tau_Ntuple"
+
+
+wait_all_job() {
+  while true; do
+    # Get the number of jobs (excluding the header line)
+    job_count=$(bjobs 2>/dev/null | tail -n +2 | wc -l)
+  
+    if [[ $job_count -eq 0 ]]; then
+      echo "No remaining jobs."
+      break
+    else
+      echo "Currently, there are $job_count job(s) running. Checking again in 5 minutes..."
+    fi
+
+    # Wait for 5 minutes
+    sleep 300
+done
+}
+
+wait_job() {
+  JOBNAME=$1
+  while true; do
+    # Filter jobs with the JOBNAME and count them
+    job_count=$(bjobs 2>/dev/null | grep -w "${JOBNAME}" | wc -l)
+
+    if [[ $job_count -eq 0 ]]; then
+      echo "No jobs with the name '${JOBNAME}' are running."
+      break
+    else
+      echo "Currently, there are $job_count job(s) with the name '${JOBNAME}' running. Checking again in 5 minutes..."
+    fi
+
+    # Wait for 5 minutes
+    sleep 300
+  done
+}
+
+bash ./submitter_Analysis_ALP.sh
+wait_job "Analyze"
+
+bash ./checker_Analysis.sh
+if [[ $? -ne 0 ]]; then
+  echo "Unsuccessful logs found. Stopping the one touch analysis."
+  exit 1
+fi
---
+++ bash/submitter_Analysis_ALP.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+submit_analysis() {
+  local Code=$1 # ex. ./bin/Analysis_main
+  local VerName=$2 # ex. Alice
+  local SampleName=$3 # ex. MUMUTAUTAU
+
+  mkdir -p "./${VerName}/${Analysis_VerName}/${SampleName}"
+  mkdir -p "./${VerName}/${Analysis_VerName}/${SampleName}/before_strict_M_deltaE_selection"
+  mkdir -p "./${VerName}/${Analysis_VerName}/${SampleName}/log"
+  mkdir -p "./${VerName}/${Analysis_VerName}/${SampleName}/err"
+
+  if compgen -G "${Ntuple_DIR}/${VerName}/${SampleName}/MC15ri/*.root" > /dev/null; then
+    for file in "${Ntuple_DIR}/${VerName}/${SampleName}/MC15ri"/*.root; do
+      filename=$(basename "$file" .root) # without path, without extension
+      bsub -q s -J Analyze -o "./${VerName}/${Analysis_VerName}/${SampleName}/log/${filename}_${SampleName}_${VerName}_${Analysis_VerName}.log" -e "./${VerName}/${Analysis_VerName}/${SampleName}/err/${filename}_${SampleName}_${VerName}_${Analysis_VerName}.err" ${Code} "${Ntuple_DIR}/${VerName}/${SampleName}/MC15ri" "${filename}.root" "./${VerName}/${Analysis_VerName}/${SampleName}"
+    done
+  fi
+
+}
+
+Types=("CHG" "MIX" "UUBAR" "DDBAR" "SSBAR" "CHARM" "MUMU" "EE" "EEEE" "EEMUMU" "EEPIPI" "EEKK" "EEPP" "PIPIISR" "PIPIPI0ISR" "KKISR" "GG" "EETAUTAU" "K0K0BARISR" "MUMUMUMU" "MUMUTAUTAU" "TAUTAUTAUTAU" "TAUPAIR" "BBs" "BsBs" "ALP")
+
+code="${Belle_tau_DIR}/analysis_code/bin/Analysis_main_ALP"
+for Type in "${Types[@]}"; do
+    submit_analysis ${code} ${Analysis_Name} ${Type}
+    sleep 0.5s
+done
