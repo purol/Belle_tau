f31ed277fd8442983c3623d0eaafc9ffe52f2816
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-01-19 21:14:56 +0900

    adjust size

--- analysis_code/src/fit_mass_ALP.cc
+++ analysis_code/src/fit_mass_ALP.cc
@@ -164,8 +164,8 @@ int main(int argc, char* argv[]) {
 
         c_M->cd();
         TPad* pad1_M = new TPad("pad1_M", "pad1_M", 0.0, 0.0, 1.0, 1.0);
-        pad1_M->SetBottomMargin(0.3); pad1_M->SetLeftMargin(0.15); pad1_M->SetGridx(); pad1_M->Draw(); pad1_M->cd();
-        M_ALP_frame->GetXaxis()->SetLabelSize(0.1); M_ALP_frame->GetXaxis()->SetTitleSize(0.1);
+        pad1_M->SetBottomMargin(0.1); pad1_M->SetLeftMargin(0.15); pad1_M->SetGridx(); pad1_M->Draw(); pad1_M->cd();
+        M_ALP_frame->GetXaxis()->SetLabelSize(0.03); M_ALP_frame->GetXaxis()->SetTitleSize(0.03);
         M_ALP_frame->Draw();
         TLegend* legend_M = new TLegend(0.2, 0.75, 0.45, 0.85);
         legend_M->AddEntry("signal MC", "signal MC", "lpe");
