61a462bda936f2db4b3ac50e9f3e406d7d93f018
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-02-06 19:54:53 +0900

    use LoadWithCut in File_Spliter

--- analysis_code/src/File_Splitter.cc
+++ analysis_code/src/File_Splitter.cc
@@ -19,7 +19,8 @@ int main(int argc, char* argv[]) {
 
     Loader loader("tau_lfv");
 
-    loader.Load(argv[1], argv[2], "label");
+    // loader.Load(argv[1], argv[2], "label");
+    loader.LoadWithCut(argv[1], argv[2], "label", "(0.5 < extraInfo__bodecayModeID__bc) && (extraInfo__bodecayModeID__bc < 1.5)");
 
     loader.RandomEventSelection(atoi(argv[3]), atoi(argv[4]));
 
