dff099725e23276043c6e068e58fa63746455f04
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-04-19 18:07:31 +0900

    large scan for CLs for large lifetime

--- bash/submitter_CLs_ALP.sh
+++ bash/submitter_CLs_ALP.sh
@@ -17,7 +17,17 @@ submit_CLs() {
     mkdir -p "./${VerName}/${Analysis_VerName}/CLs_${mass}_${life}_${A}_${B}/out"
     mkdir -p "./${VerName}/${Analysis_VerName}/CLs_${mass}_${life}_${A}_${B}/log"
 
-    for mu in $(seq 0 0.1 5.0)
+    # life 값이 100 이상인지 awk를 이용해 확인 (소수점, 지수 표기법 처리 가능)
+    is_large_life=$(awk -v life="$life" 'BEGIN { if (life >= 100) print 1; else print 0 }')
+
+    # 조건에 따라 mu 반복 범위(seq) 설정
+    if [ "$is_large_life" -eq 1 ]; then
+      mu_list=$(seq 0 1 50)     # life가 100 이상일 때 (0부터 50까지 1단위)
+    else
+      mu_list=$(seq 0 0.1 5.0)  # life가 100 미만일 때 (0부터 5.0까지 0.1단위)
+    fi
+
+    for mu in $mu_list
     do
       for index in {0..10}; do
         bsub -q s -J TAUCLS -o "./${VerName}/${Analysis_VerName}/CLs_${mass}_${life}_${A}_${B}/log/${mu}_${index}.log" ${Code} "./${VerName}/${Analysis_VerName}" "workspace_${mass}_${life}_${A}_${B}.root" "./${VerName}/${Analysis_VerName}/CLs_${mass}_${life}_${A}_${B}/out" ${mu} ${index}
@@ -31,4 +41,3 @@ submit_CLs() {
 
 code="${Belle_tau_DIR}/analysis_code/bin/Run_CLs"
 submit_CLs ${code} ${Analysis_Name}
-
