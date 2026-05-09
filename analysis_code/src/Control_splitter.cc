d4aa411766fa3f787fa1ab6cfe71274fef5b7727
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-05-09 19:03:12 +0900

    add control sample splitter

---
+++ analysis_code/src/Control_splitter.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+#include "TFile.h"
+
+#include "Loader.h"
+#include "constants.h"
+#include "MyObtainWeight.h"
+
+int main(int argc, char* argv[]) {
+    /*
+    * argv[1]: dirname
+    * argv[2]: filename
+    * argv[3]: signal output path
+    * argv[4]: background output path
+    */
+
+    Loader toSignal("tau_lfv");
+    toSignal.LoadWithCut(argv[1], argv[2], "label", "((111.5 < tauPlusMCMode) && (tauPlusMCMode < 112.5)) || ((225.5 < tauPlusMCMode) && (tauPlusMCMode < 226.5))");
+    toSignal.PrintSeparateRootFile(argv[3], "SIGNAL_", "");
+    toSignal.end();
+
+    Loader toTau("tau_lfv");
+    toTau.LoadWithCut(argv[1], argv[2], "label", "((111.5 > tauPlusMCMode) || (tauPlusMCMode > 112.5)) && ((225.5 > tauPlusMCMode) || (tauPlusMCMode > 226.5))");
+    toTau.PrintSeparateRootFile(argv[4], "TAUPAIR_", "");
+    toTau.end();
+
+    return 0;
+}
