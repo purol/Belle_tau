3a4b6d4970fe4a314720b01ba4a949e3ed9ee2c6
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-05-14 18:50:38 +0900

    add script for skimming MC16ri

---
+++ bash_SKIM/SKIM_MC16ri_all.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p CCBAR_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_ccbar_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p CHG_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_charged_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p DDBAR_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_ddbar_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p EE_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_ee_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p EEEE_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_eeee_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p EEMUMU_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_eemumu_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p GG_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_gg_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p HHISR_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_hhISR_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p LLXX_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_llxx_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p MIX_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_mixed_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p MUMU_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_mumu_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p SSBAR_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_ssbar_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p TAUPAIR_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_taupair_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p UUBAR_SKIM_16i_1 -i /belle/collection/MC/MC16ri_run1_uubar_4S_v1 -s light-2511-gacrux --cputime 1600
+
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p CCBAR_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_ccbar_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p CHG_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_charged_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p DDBAR_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_ddbar_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p EE_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_ee_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p EEEE_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_eeee_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p EEMUMU_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_eemumu_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p GG_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_gg_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p HHISR_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_hhISR_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p LLXX_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_llxx_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p MIX_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_mixed_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p MUMU_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_mumu_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p SSBAR_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_ssbar_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p TAUPAIR_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_taupair_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p UUBAR_SKIM_16i_2 -i /belle/collection/MC/MC16ri_run2_PXDoff_uubar_4S_v1 -s light-2511-gacrux --cputime 1600
+
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p CCBAR_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_ccbar_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p CHG_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_charged_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p DDBAR_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_ddbar_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p EE_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_ee_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p EEEE_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_eeee_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p EEMUMU_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_eemumu_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p GG_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_gg_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p HHISR_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_hhISR_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p LLXX_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_llxx_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p MIX_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_mixed_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p MUMU_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_mumu_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p SSBAR_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_ssbar_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p TAUPAIR_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_taupair_4S_v1 -s light-2511-gacrux --cputime 1600
+gbasf2 ./python/tau_mumumu_TauToMuMuMu.py --force -p UUBAR_SKIM_16i_3 -i /belle/collection/MC/MC16ri_run2_PXDon_uubar_4S_v1 -s light-2511-gacrux --cputime 1600
