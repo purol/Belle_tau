0a4c8e8acb18b573203d888c1d10095ccb3edf41
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-05-20 13:47:41 +0900

    change CLs scan range

--- bash/submitter_CLs_ALP.sh
+++ bash/submitter_CLs_ALP.sh
@@ -21,6 +21,8 @@ submit_CLs() {
       mu_list="$(seq 0 10 300)"
     elif awk -v life="$life" 'BEGIN { exit !(life >= 300 && life < 700) }'; then
       mu_list="$(seq 0 10 200)"
+    elif awk -v life="$life" 'BEGIN { exit !(life >= 50 && life < 300) }'; then
+      mu_list="$(seq 0 0.2 10.0)"
     else
       mu_list="$(seq 0 0.1 5.0)"
     fi
