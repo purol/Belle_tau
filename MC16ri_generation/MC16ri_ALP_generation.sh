fce13abfcd7224dc4df1c057df3d1aacd7df6436
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-05-26 17:32:36 +0900

    add gen code for MC16ri ALP

---
+++ MC16ri_generation/MC16ri_ALP_generation.sh
@@ -0,0 +1,104 @@
+#!/bin/sh
+
+DAT_DIR_PLUS="./dat_files_plus"
+DAT_DIR_MINUS="./dat_files_minus"
+
+mkdir -p ./output
+mkdir -p ./log
+mkdir -p ./err
+
+for datfile in ${DAT_DIR_PLUS}/*.dat; do
+    for iterator in {1..5}; do
+        # Extract parameters from filename: alpha_mass0.2_life0.1_A1_B1.dat
+        mass=$(echo $datfile | grep -oP 'mass\K[0-9.]+')
+        lifetime=$(echo $datfile | grep -oP 'life\K[0-9.]+')
+        A=$(echo $datfile | grep -oP 'A\K-?[01]')
+        B=$(echo $datfile | grep -oP 'B\K-?[01]')
+
+        nopath_datfile=$(basename "$datfile")
+
+        echo "Generating MC for $datfile"
+        bsub -q l -o "./log/${nopath_datfile}_${iterator}.log" ./python/MC16ri_run1_gen.py --decfile ${datfile} --outputfile "./output/MC16ri_run1_plus_alpha_mass${mass}_life${lifetime}_A${A}_B${B}_${iterator}.root"
+        sleep 0.5
+    done
+done
+
+for datfile in ${DAT_DIR_PLUS}/*.dat; do
+    for iterator in {1..3}; do
+        # Extract parameters from filename: alpha_mass0.2_life0.1_A1_B1.dat
+        mass=$(echo $datfile | grep -oP 'mass\K[0-9.]+')
+        lifetime=$(echo $datfile | grep -oP 'life\K[0-9.]+')
+        A=$(echo $datfile | grep -oP 'A\K-?[01]')
+        B=$(echo $datfile | grep -oP 'B\K-?[01]')
+
+        nopath_datfile=$(basename "$datfile")
+
+        echo "Generating MC for $datfile"
+        bsub -q l -o "./log/${nopath_datfile}_${iterator}.log" ./python/MC16ri_run2_PXD_OFF_gen.py --decfile ${datfile} --outputfile "./output/MC16ri_run2_PXD_OFF_plus_alpha_mass${mass}_life${lifetime}_A${A}_B${B}_${iterator}.root"
+        sleep 0.5
+    done
+done
+
+for datfile in ${DAT_DIR_PLUS}/*.dat; do
+    for iterator in {1..3}; do
+        # Extract parameters from filename: alpha_mass0.2_life0.1_A1_B1.dat
+        mass=$(echo $datfile | grep -oP 'mass\K[0-9.]+')
+        lifetime=$(echo $datfile | grep -oP 'life\K[0-9.]+')
+        A=$(echo $datfile | grep -oP 'A\K-?[01]')
+        B=$(echo $datfile | grep -oP 'B\K-?[01]')
+
+        nopath_datfile=$(basename "$datfile")
+
+        echo "Generating MC for $datfile"
+        bsub -q l -o "./log/${nopath_datfile}_${iterator}.log" ./python/MC16ri_run2_PXD_ON_gen.py --decfile ${datfile} --outputfile "./output/MC16ri_run2_PXD_ON_plus_alpha_mass${mass}_life${lifetime}_A${A}_B${B}_${iterator}.root"
+        sleep 0.5
+    done
+done
+
+for datfile in ${DAT_DIR_MINUS}/*.dat; do
+    for iterator in {1..5}; do
+        # Extract parameters from filename: alpha_mass0.2_life0.1_A1_B1.dat
+        mass=$(echo $datfile | grep -oP 'mass\K[0-9.]+')
+        lifetime=$(echo $datfile | grep -oP 'life\K[0-9.]+')
+        A=$(echo $datfile | grep -oP 'A\K-?[01]')
+        B=$(echo $datfile | grep -oP 'B\K-?[01]')
+
+        nopath_datfile=$(basename "$datfile")
+
+        echo "Generating MC for $datfile"
+        bsub -q l -o "./log/${nopath_datfile}_${iterator}.log" ./python/MC16ri_run1_gen.py --decfile ${datfile} --outputfile "./output/MC16ri_run1_minus_alpha_mass${mass}_life${lifetime}_A${A}_B${B}_${iterator}.root"
+        sleep 0.5
+    done
+done
+
+for datfile in ${DAT_DIR_MINUS}/*.dat; do
+    for iterator in {1..3}; do
+        # Extract parameters from filename: alpha_mass0.2_life0.1_A1_B1.dat
+        mass=$(echo $datfile | grep -oP 'mass\K[0-9.]+')
+        lifetime=$(echo $datfile | grep -oP 'life\K[0-9.]+')
+        A=$(echo $datfile | grep -oP 'A\K-?[01]')
+        B=$(echo $datfile | grep -oP 'B\K-?[01]')
+
+        nopath_datfile=$(basename "$datfile")
+
+        echo "Generating MC for $datfile"
+        bsub -q l -o "./log/${nopath_datfile}_${iterator}.log" ./python/MC16ri_run2_PXD_OFF_gen.py --decfile ${datfile} --outputfile "./output/MC16ri_run2_PXD_OFF_minus_alpha_mass${mass}_life${lifetime}_A${A}_B${B}_${iterator}.root"
+        sleep 0.5
+    done
+done
+
+for datfile in ${DAT_DIR_MINUS}/*.dat; do
+    for iterator in {1..3}; do
+        # Extract parameters from filename: alpha_mass0.2_life0.1_A1_B1.dat
+        mass=$(echo $datfile | grep -oP 'mass\K[0-9.]+')
+        lifetime=$(echo $datfile | grep -oP 'life\K[0-9.]+')
+        A=$(echo $datfile | grep -oP 'A\K-?[01]')
+        B=$(echo $datfile | grep -oP 'B\K-?[01]')
+
+        nopath_datfile=$(basename "$datfile")
+
+        echo "Generating MC for $datfile"
+        bsub -q l -o "./log/${nopath_datfile}_${iterator}.log" ./python/MC16ri_run2_PXD_ON_gen.py --decfile ${datfile} --outputfile "./output/MC16ri_run2_PXD_ON_minus_alpha_mass${mass}_life${lifetime}_A${A}_B${B}_${iterator}.root"
+        sleep 0.5
+    done
+done
--- MC16ri_generation/dat_generator.py
+++ MC16ri_generation/dat_generator.py
@@ -1,16 +1,17 @@
 import os
 
 # Parameter sets
-masses = [round(0.3 + 0.2 * i, 1) for i in range(0, 7)]  # 0.2 to 1.6 GeV
+masses = [round(0.3 + 0.2 * i, 1) for i in range(0, 7)]  # 0.3 to 1.5 GeV
 lifetimes = [0.1, 1, 10, 100, 250, 500, 1000]  # mm
 coefficients = [(1, -1)]  # (A_COEF, B_COEF)
 
 # Directories
 dat_dir = "dat_files"
-os.makedirs(dat_dir, exist_ok=True)
+os.makedirs(dat_dir + "_plus", exist_ok=True)
+os.makedirs(dat_dir + "_minus", exist_ok=True)
 
 # Template
-dat_template = """0.08711                         * c*tau(tau life time) (mm) 
+dat_template_plus = """0.08711                         * c*tau(tau life time) (mm) 
 1                               * switch for long lived (1:no decay, 0:decay)
 1                               * switch for KKMC-JETSET (1:on, 0:off)
 ********** Above is for basf *********
@@ -63,13 +64,78 @@ BeginX
 EndX
 """
 
+dat_template_minus = """0.08711                         * c*tau(tau life time) (mm) 
+1                               * switch for long lived (1:no decay, 0:decay)
+1                               * switch for KKMC-JETSET (1:on, 0:off)
+********** Above is for basf *********
+
+* MCEventType: 341cltnkeu Descriptor: tau+ -> mu+alpha [and cc]
+* alpha -> mu- mu+ [alpha: pseudoscalar, mass 1 GeV, width 4 MeV, ctau = 50 cm]
+
+BeginX
+********************************************************************************
+*               ACTUAL DATA FOR THIS PARTICULAR RUN
+********************************************************************************
+*indx_______________ccccccccc0ccccccccc0ccccccccc0ccccccccc0ccccccccc0ccccccccc0
+*     Center-of-mass energy [GeV]
+    1        10.58D0      CMSene =xpar( 1) Average Center of mass energy [GeV]
+    2        0.007e0      DelEne =xpar( 2)  Beam energy spread [GeV]
+*   5             -1
+********************************************************************************
+*   61       0.7071d0      spin1x  polarization vector beam 1
+*   62            0d0      spin1y  polarization vector beam 1
+*   63       0.7071d0      spin1z  polarization vector beam 1
+*   64            0d0      spin1x  polarization vector beam 2
+*   65      -0.7071d0      spin1y  polarization vector beam 2
+*   66       0.7071d0      spin1z  polarization vector beam 2
+********************************************************************************
+*     Define process
+  415              1      KFfin, Tau
+********************************************************************************
+  901              4      Ihvp  ! =1,2,3,4
+********************************************************************************
+ 2001          373e0      Jak1   =xpar(71)  First  Tau decay mask (tau-)
+ 2002            0e0      Jak2   =xpar(72)  Second Tau decay mask (tau+)
+********************************************************************************
+ 2900            1e0      BBB    0: ORIG  1: BBB
+ 2901            0e0      EQUALBR
+ 2902            2e0      FF2PIRHO
+ 2903            1e0      IRCHL3PI
+ 2904            0e0      IFKPIPI
+ 2905            0e0      IFCURR4PI
+ 2909            1e0      CHCONJUGATE = 1: swap JAK1/JAK2 for alternate events
+ 2910            0e0      TauToElOrMu = 1: swap e/mu decay for alternate events
+********************************************************************************
+ 2911{mass:>13.3f}d0      AMALFA: mass of exotic axion-like particle [GeV]
+ 2912            0e0      ISALFA: spin of exotic axion-like particle
+ 2913{a_coef:>13.1f}d0      VCALFA: coupling coefficient A in (A+B gamma5) term
+ 2914{b_coef:>13.1f}d0      ACALFA: coupling coefficient B in (A+B gamma5) term
+ 2920            3e0      0:invi,1:gg,2:e+e-,3:mu+mu-,4:pi+pi-,5:pi0pi0,6:pi0eta,7:etaeta,8:K+K-,9:KS0KS0,10:pi+K-,11:pi-K+
+ 2921        0.001d0      GMALFA: width of the exotic particle [GeV]
+ 2922{lifetime:>13.3f}d0      CTALFA : c*tau (alpha lifetime) (mm)
+********************************************************************************
+EndX
+"""
+
 dat_files = []
 for mass in masses:
     for lifetime in lifetimes:
         for a_coef, b_coef in coefficients:
-            filename = f"{dat_dir}/alpha_mass{mass:.1f}_life{lifetime}_A{a_coef}_B{b_coef}_plus.dat"
+            filename = f"{dat_dir}_plus/alpha_mass{mass:.1f}_life{lifetime}_A{a_coef}_B{b_coef}_plus.dat"
             with open(filename, "w") as f:
-                f.write(dat_template.format(mass=mass, lifetime=lifetime, a_coef=a_coef, b_coef=b_coef))
+                f.write(dat_template_plus.format(mass=mass, lifetime=lifetime, a_coef=a_coef, b_coef=b_coef))
             dat_files.append(filename)
 
-print(f"Generated {len(dat_files)} .dat files in {dat_dir}/")
+print(f"Generated {len(dat_files)} .dat files in {dat_dir}_plus/")
+
+
+dat_files = []
+for mass in masses:
+    for lifetime in lifetimes:
+        for a_coef, b_coef in coefficients:
+            filename = f"{dat_dir}_minus/alpha_mass{mass:.1f}_life{lifetime}_A{a_coef}_B{b_coef}_minus.dat"
+            with open(filename, "w") as f:
+                f.write(dat_template_minus.format(mass=mass, lifetime=lifetime, a_coef=a_coef, b_coef=b_coef))
+            dat_files.append(filename)
+
+print(f"Generated {len(dat_files)} .dat files in {dat_dir}_minus/")
