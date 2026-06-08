c791c5603c957c9fcfb9c58adbe5361c1f978a75
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-06-09 01:51:10 +0900

    fix splitter

--- analysis_code/src/Control_splitter_Konpaku_patch.cc
+++ analysis_code/src/Control_splitter_Konpaku_patch.cc
@@ -16,10 +16,29 @@ int main(int argc, char* argv[]) {
     * argv[4]: background output path
     */
 
-    Loader toSignal("tau_lfv");
-    toSignal.LoadWithCut(argv[1], argv[2], "label", "(nParticlesInList__botau__pl__clpipipi__bc > 0.5) || (nParticlesInList__botau__pl__cldirect__bc > 0.5)");
-    toSignal.PrintSeparateRootFile(argv[3], "SIGNAL_", "");
-    toSignal.end();
+    Loader topipipinu("tau_lfv");
+    topipipinu.LoadWithCut(argv[1], argv[2], "label", "(nParticlesInList__botau__pl__clpipipi__bc > 0.5) || (nParticlesInList__botau__pl__cldirect__bc > 0.5)");
+    topipipinu.LoadWithCut(argv[1], argv[2], "label", "nParticlesInList__botau__pl__clpipipi__bc > 0.5");
+    topipipinu.PrintSeparateRootFile(argv[3], "pipipinu_", "");
+    topipipinu.end();
+
+    Loader toKS0pinu("tau_lfv");
+    toKS0pinu.LoadWithCut(argv[1], argv[2], "label", "(nParticlesInList__botau__pl__clpipipi__bc > 0.5) || (nParticlesInList__botau__pl__cldirect__bc > 0.5)");
+    toKS0pinu.LoadWithCut(argv[1], argv[2], "label", "nParticlesInList__botau__pl__cldirect__bc > 0.5");
+    toKS0pinu.PrintSeparateRootFile(argv[3], "KS0pinu_", "");
+    toKS0pinu.end();
+
+    Loader topurepipipinu("tau_lfv");
+    topurepipipinu.LoadWithCut(argv[1], argv[2], "label", "(nParticlesInList__botau__pl__clpipipi__bc > 0.5) || (nParticlesInList__botau__pl__cldirect__bc > 0.5)");
+    topurepipipinu.LoadWithCut(argv[1], argv[2], "label", "nParticlesInList__botau__pl__cldirect__bc < 0.5");
+    topurepipipinu.PrintSeparateRootFile(argv[3], "pure_pipipinu_", "");
+    topurepipipinu.end();
+
+    Loader topureKS0pinu("tau_lfv");
+    topureKS0pinu.LoadWithCut(argv[1], argv[2], "label", "(nParticlesInList__botau__pl__clpipipi__bc > 0.5) || (nParticlesInList__botau__pl__cldirect__bc > 0.5)");
+    topureKS0pinu.LoadWithCut(argv[1], argv[2], "label", "nParticlesInList__botau__pl__clpipipi__bc < 0.5");
+    topureKS0pinu.PrintSeparateRootFile(argv[3], "pure_KS0pinu_", "");
+    topureKS0pinu.end();
 
     Loader toTau("tau_lfv");
     toTau.LoadWithCut(argv[1], argv[2], "label", "(nParticlesInList__botau__pl__clpipipi__bc < 0.5) && (nParticlesInList__botau__pl__cldirect__bc < 0.5)");
