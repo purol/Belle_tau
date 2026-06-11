997272885b4514e65cfce6158d5793a4888ae91e
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-06-11 12:37:45 +0900

    bug fix

--- analysis_code/src/Control_splitter.cc
+++ analysis_code/src/Control_splitter.cc
@@ -18,25 +18,25 @@ int main(int argc, char* argv[]) {
 
     Loader topipipinu("tau_lfv");
     topipipinu.LoadWithCut(argv[1], argv[2], "label", "(((111.5 < tauPlusMCMode) && (tauPlusMCMode < 112.5)) || ((225.5 < tauPlusMCMode) && (tauPlusMCMode < 226.5))) || (((111.5 < tauMinusMCMode) && (tauMinusMCMode < 112.5)) || ((225.5 < tauMinusMCMode) && (tauMinusMCMode < 226.5)))");
-    topipipinu.LoadWithCut(argv[1], argv[2], "label", "((111.5 < tauPlusMCMode) && (tauPlusMCMode < 112.5)) || ((111.5 < tauMinusMCMode) && (tauMinusMCMode < 112.5))");
+    topipipinu.Cut("((111.5 < tauPlusMCMode) && (tauPlusMCMode < 112.5)) || ((111.5 < tauMinusMCMode) && (tauMinusMCMode < 112.5))");
     topipipinu.PrintSeparateRootFile(argv[3], "pipipinu_", "");
     topipipinu.end();
 
     Loader toKS0pinu("tau_lfv");
     toKS0pinu.LoadWithCut(argv[1], argv[2], "label", "(((111.5 < tauPlusMCMode) && (tauPlusMCMode < 112.5)) || ((225.5 < tauPlusMCMode) && (tauPlusMCMode < 226.5))) || (((111.5 < tauMinusMCMode) && (tauMinusMCMode < 112.5)) || ((225.5 < tauMinusMCMode) && (tauMinusMCMode < 226.5)))");
-    toKS0pinu.LoadWithCut(argv[1], argv[2], "label", "((225.5 < tauPlusMCMode) && (tauPlusMCMode < 226.5)) || ((225.5 < tauMinusMCMode) && (tauMinusMCMode < 226.5))");
+    toKS0pinu.Cut("((225.5 < tauPlusMCMode) && (tauPlusMCMode < 226.5)) || ((225.5 < tauMinusMCMode) && (tauMinusMCMode < 226.5))");
     toKS0pinu.PrintSeparateRootFile(argv[3], "KS0pinu_", "");
     toKS0pinu.end();
 
     Loader topurepipipinu("tau_lfv");
     topurepipipinu.LoadWithCut(argv[1], argv[2], "label", "(((111.5 < tauPlusMCMode) && (tauPlusMCMode < 112.5)) || ((225.5 < tauPlusMCMode) && (tauPlusMCMode < 226.5))) || (((111.5 < tauMinusMCMode) && (tauMinusMCMode < 112.5)) || ((225.5 < tauMinusMCMode) && (tauMinusMCMode < 226.5)))");
-    topurepipipinu.LoadWithCut(argv[1], argv[2], "label", "((225.5 > tauPlusMCMode) || (tauPlusMCMode > 226.5)) && ((225.5 > tauMinusMCMode) || (tauMinusMCMode > 226.5))");
+    topurepipipinu.Cut("((225.5 > tauPlusMCMode) || (tauPlusMCMode > 226.5)) && ((225.5 > tauMinusMCMode) || (tauMinusMCMode > 226.5))");
     topurepipipinu.PrintSeparateRootFile(argv[3], "pure_pipipinu_", "");
     topurepipipinu.end();
 
     Loader topureKS0pinu("tau_lfv");
     topureKS0pinu.LoadWithCut(argv[1], argv[2], "label", "(((111.5 < tauPlusMCMode) && (tauPlusMCMode < 112.5)) || ((225.5 < tauPlusMCMode) && (tauPlusMCMode < 226.5))) || (((111.5 < tauMinusMCMode) && (tauMinusMCMode < 112.5)) || ((225.5 < tauMinusMCMode) && (tauMinusMCMode < 226.5)))");
-    topureKS0pinu.LoadWithCut(argv[1], argv[2], "label", "((111.5 > tauPlusMCMode) || (tauPlusMCMode > 112.5)) && ((111.5 > tauMinusMCMode) || (tauMinusMCMode > 112.5))");
+    topureKS0pinu.Cut("((111.5 > tauPlusMCMode) || (tauPlusMCMode > 112.5)) && ((111.5 > tauMinusMCMode) || (tauMinusMCMode > 112.5))");
     topureKS0pinu.PrintSeparateRootFile(argv[3], "pure_KS0pinu_", "");
     topureKS0pinu.end();
 
