c93954cd9e1ea75dba0a5347f66249dd22242ac2
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-07-22 18:51:16 +0900

    gen_cut_KEK_study.sh

---
+++ bash_generator_cut/gen_cut_KEK_study.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+    echo "usage: gen_cut_KEK_study.sh {input dirname} {output path}"
+    exit 1
+fi
+
+input_dir=${1}
+output_dir=${2}
+
+mkdir -p "${output_dir}/output"
+mkdir -p "${output_dir}/log"
+mkdir -p "${output_dir}/err"
+
+if compgen -G "${input_dir}/*.root" > /dev/null; then
+  for file in "${input_dir}"/*.root; do
+    filename=$(basename "$file" .root) # without path, without extension
+    bsub -q s -J GenCutStudy -o "${output_dir}/log/${filename}.log" -e "${output_dir}/err/${filename}.err" "./python/gbasf2_Kanako.py" --sample "MC15ri" --type "uubar" --energy "4S" --prompt --vertex --gencut --KEKCC --inputfile "${file}"  --destination "${output_dir}/output" 
+  done
+fi
