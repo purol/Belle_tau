ea9d37752be742606700c6e03a10f8aacebaadd5
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-07-28 02:09:09 +0900

    bug fix

--- bash/submitter_FBDT_comparison_tagging_CTRL.sh
+++ bash/submitter_FBDT_comparison_tagging_CTRL.sh
@@ -32,8 +32,8 @@ submit_Plotter() {
  
 code="${Belle_tau_DIR}/analysis_code/bin/var_comparison_tag"
 VarName="BDT_output_1"
-submit_Plotter ${code} ${Analysis_Name} ${VarName} 0.0 1.0 "before_leptonic_tag" "FBDT1_comp_tag" "plot" "${Signal_Type}"
+submit_Plotter ${code} ${Analysis_Name} ${VarName} 0.0 1.0 "before_leptonic_tag_after_application" "FBDT1_comp_tag" "plot" "${Signal_Type}"
 
 code="${Belle_tau_DIR}/analysis_code/bin/var_comparison_tag"
 VarName="BDT_output_2"
-submit_Plotter ${code} ${Analysis_Name} ${VarName} 0.0 1.0 "before_leptonic_tag" "FBDT2_comp_tag" "plot" "${Signal_Type}"
+submit_Plotter ${code} ${Analysis_Name} ${VarName} 0.0 1.0 "before_leptonic_tag_after_application" "FBDT2_comp_tag" "plot" "${Signal_Type}"
--- bash/submitter_FBDT_comparison_tagging_CTRL_ALP.sh
+++ bash/submitter_FBDT_comparison_tagging_CTRL_ALP.sh
@@ -47,8 +47,8 @@ submit_Plotter() {
  
 code="${Belle_tau_DIR}/analysis_code/bin/var_comparison_tag"
 VarName="BDT_output_1"
-submit_Plotter ${code} ${Analysis_Name} ${VarName} 0.0 1.0 "before_leptonic_tag" "final_output_test_after_application" "FBDT1_comp_tag" "plot" "${Signal_Type}" "ALP"
+submit_Plotter ${code} ${Analysis_Name} ${VarName} 0.0 1.0 "before_leptonic_tag_after_application" "final_output_test_after_application" "FBDT1_comp_tag" "plot" "${Signal_Type}" "ALP"
 
 code="${Belle_tau_DIR}/analysis_code/bin/var_comparison_tag"
 VarName="BDT_output_2"
-submit_Plotter ${code} ${Analysis_Name} ${VarName} 0.0 1.0 "before_leptonic_tag" "final_output_test_after_application" "FBDT2_comp_tag" "plot" "${Signal_Type}" "ALP"
+submit_Plotter ${code} ${Analysis_Name} ${VarName} 0.0 1.0 "before_leptonic_tag_after_application" "final_output_test_after_application" "FBDT2_comp_tag" "plot" "${Signal_Type}" "ALP"
