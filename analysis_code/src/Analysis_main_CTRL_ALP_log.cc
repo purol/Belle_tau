fb6fd37088c06e140f073f1eceedb9e444b8412c
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-08-11 15:21:36 +0900

    bug fix

--- analysis_code/src/Analysis_main_CTRL_ALP_log.cc
+++ analysis_code/src/Analysis_main_CTRL_ALP_log.cc
@@ -46,6 +46,12 @@ std::map<std::string, std::string> momentum_theta = {
     {"extraInfo__boThreeMuon_p__bc", "extraInfo__boThreeMuon_theta__bc"}
 };
 
+std::map<std::string, std::string> momentum_charge = {
+    {"extraInfo__boOneMuon_p__bc", "extraInfo__boOneMuon_charge__bc"},
+    {"extraInfo__boTwoMuon_p__bc", "extraInfo__boTwoMuon_charge__bc"},
+    {"extraInfo__boThreeMuon_p__bc", "extraInfo__boThreeMuon_charge__bc"}
+};
+
 std::vector<std::string> cosToThrustOfEvent_CM = {
     "extraInfo__boOneMuon_cosToThrustOfEvent__bc",
     "extraInfo__boTwoMuon_cosToThrustOfEvent__bc",
