478a6009faa69214cf8b46cc844a17b27fdf838e
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-08-12 17:40:09 +0900

    bug fix on bash script

--- bash/submitter_FBDT_comparison_CTRL2_ALP.sh
+++ bash/submitter_FBDT_comparison_CTRL2_ALP.sh
@@ -60,10 +60,10 @@ submit_Plotter() {
 
 }
  
-code="${Belle_tau_DIR}/analysis_code/bin/var_comparison_CTRL_ALP_one"
+code="${Belle_tau_DIR}/analysis_code/bin/var_comparison_CTRL2_ALP_one"
 VarName="BDT_output_1"
 submit_Plotter ${code} ${Analysis_Name} ${VarName} 0.0 1.0 "final_output_after_application" "final_output_test_after_application" "FBDT1_comp" "plot" "${Background_Types_STR}" "${Background_Types_STR}"
 
-code="${Belle_tau_DIR}/analysis_code/bin/var_comparison_CTRL_ALP_two"
+code="${Belle_tau_DIR}/analysis_code/bin/var_comparison_CTRL2_ALP_two"
 VarName="BDT_output_2"
 submit_Plotter ${code} ${Analysis_Name} ${VarName} 0.0 1.0 "final_output_after_application" "final_output_test_after_application" "FBDT2_comp" "plot" "${Background_Types_STR}" "${Background_Types_STR}"
