66d3f0108177b67686311de55a0f2e6520ef5f7d
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-08-15 03:55:26 +0900

    bug fix

--- generator_cut_private/uubar_eph3_MC16ri_run2_PXD_ON_gencut.py
+++ generator_cut_private/uubar_eph3_MC16ri_run2_PXD_ON_gencut.py
@@ -28,6 +28,7 @@ def AnalysisGenCut(path):
     # copy from Doremy
     import modularAnalysis as ma
     import variables as va
+    import variables.collections as vc
 
     MDeltaCuts = ["", "[1.0 < M < 4.0] and [-1.0 < deltaE < 1.0]", "[1.0 < M < 2.5] and [-1.0 < deltaE < 1.0]"]
 
