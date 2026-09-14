3e2887e0b608500e04a7aaafce719a4399fbeb6e
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-09-15 02:04:55 +0900

    bug fix

--- analysis_code/src/Analysis_main.cc
+++ analysis_code/src/Analysis_main.cc
@@ -76,7 +76,7 @@ int main(int argc, char* argv[]) {
     Loader loader("tau_lfv");
 
     // It is prompt decay analysis
-    loader.LoadWithCut(argv[1], "root", "label", "(0.5 < extraInfo__bodecayModeID__bc) && (extraInfo__bodecayModeID__bc < 1.5)");
+    loader.LoadWithCut(argv[1], argv[2], "label", "(0.5 < extraInfo__bodecayModeID__bc) && (extraInfo__bodecayModeID__bc < 1.5)");
     loader.AddWeight("MC_weight", { {"MySampleType", "MySampleType"}, {"MyEventType", "MyEventType"}, {"MyEnergyType", "MyEnergyType"}, {"MyALPLife", "MyALPLife"} });
 
     loader.ConditionalPairDefineNewVariable(momentum_muonmomentum, 0, "first_muon_p");
