a2003927a8fbff6efb9e2b5f3dc73957a3e5bede
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2026-09-22 10:53:41 +0900

    fix control split fix

--- bash/submitter_control_splitter.sh
+++ bash/submitter_control_splitter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export Analysis_Name="Konpaku_CTRL"
+export Analysis_Name="Larva_CTRL"
 export Analysis_VerName="v000"
 
 export Belle_tau_DIR="/home/belle2/junewoo/storage_b2/tau_workspace/Belle_tau"
@@ -16,15 +16,23 @@ submit_splitter() {
   mkdir -p "./${VerName}/${Analysis_VerName}/${SampleName}/log"
   mkdir -p "./${VerName}/${Analysis_VerName}/${SampleName}/err"
 
-  for file in "${Ntuple_DIR}/${VerName}/${SampleName}/MC15ri"/*.root; do
+  for file in "${Ntuple_DIR}/${VerName}/${SampleName}/MC16rd"/*.root; do
     filename=$(basename "$file" .root) # without path, without extension
-    bsub -q s -J Split -o "./${VerName}/${Analysis_VerName}/${SampleName}/log/${filename}.log" -e "./${VerName}/${Analysis_VerName}/${SampleName}/err/${filename}.err" ${Code} "${Ntuple_DIR}/${VerName}/${SampleName}/MC15ri" "${filename}.root" "./${VerName}/${Analysis_VerName}/${SampleName}/output/" "./${VerName}/${Analysis_VerName}/${SampleName}/output/"
+    bsub -q s \
+    -J Split \
+    -o "./${VerName}/${Analysis_VerName}/${SampleName}/log/${filename}.log" \
+    -e "./${VerName}/${Analysis_VerName}/${SampleName}/err/${filename}.err" \
+    ${Code} \
+    "${Ntuple_DIR}/${VerName}/${SampleName}/MC16rd" \
+    "${filename}.root" \
+    "./${VerName}/${Analysis_VerName}/${SampleName}/output/" \
+    "./${VerName}/${Analysis_VerName}/${SampleName}/output/"
     sleep 0.3s
   done
 
 }
 
-Types=("TAUPAIR_original_notOF")
+Types=("TAUPAIR_original")
 
 code="${Belle_tau_DIR}/analysis_code/bin/Control_splitter"
 for Type in "${Types[@]}"; do
